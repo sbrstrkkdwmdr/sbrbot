a4a7e9b12d331c9745289ff566b886f70716f146
SaberStrike <sabersfear@gmail.com>
2021-11-03 18:06:12 +1100

    update hentai command

--- commands/hentai.js
+++ commands/hentai.js
@@ -1,8 +1,11 @@
 module.exports = {
     name: 'hentai',
     execute(message, args) {
-        if(a) {}
+        if(message.channel.nsfw) {
         let pp = Math.floor(Math.random () * 70005 + 1)
-        message.channel.send(`https://nhentai.net/g/${w}`)
+        message.channel.send(`https://nhentai.net/g/${pp}`)}
+        else {
+            message.channel.send("this channel is not NSFW")
+        }
     }
 }
