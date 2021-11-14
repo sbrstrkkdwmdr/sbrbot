4643786a9bdddd94536c5dec253249ed2df3ad23
SaberStrike <sabersfear@gmail.com>
2021-11-15 09:33:24 +1100

    new ping command
    will add quick.db later

--- commands/ping.js
+++ commands/ping.js
@@ -1,7 +1,60 @@
-module.exports = {
+/*module.exports = {
     name: 'ping',
     description: 'ping',
     execute(message, args) {
         message.channel.send('pong!');   
     }
+}*/
+// @ts-check // Can be removed, used to check typings and valid actions
+
+const { MessageEmbed } = require('discord.js');
+const quick = require('quick.db');
+
+module.exports = {
+  name: 'ping',
+  aliases: [],
+  description: 'Get bot ping.',
+  async execute(client, message) {
+    const ping = await getDBPingData();
+    const messagePing = Date.now(); // start before message sent
+    const msg = await message.channel.send('Loading...');
+    const endMessagePing = Date.now() - messagePing; // end of message sent
+
+    const embed = new MessageEmbed() // build message embed
+      .setDescription(
+        `
+        Database ping data:
+        - Fetch ping: \`${ping.endGet}ms\`
+        - Wright ping: \`${ping.endWright}ms\`
+        - Avrage ping: \`${ping.avarage}ms\`
+        Message ping: \`${endMessagePing}ms\`
+      `
+      )
+      .setColor('GREEN')
+      .setTimestamp();
+
+    msg.edit({
+      content: '',
+      embed,
+    }); // edit message content
+  },
+};
+
+async function getDBPingData() {
+  // get the fetch data ping
+  const startGet = Date.now();
+  await quick.get('QR=.');
+  const endGet = Date.now() - startGet;
+
+  // get the wright data ping
+  const startWright = Date.now();
+  await quick.set('QR=.', Buffer.from(startWright.toString()).toString('base64'));
+  const endWright = Date.now() - startWright;
+
+  // avrage ping time
+  const avarage = (endGet + endWright) / 2;
+  try {
+    quick.delete('QR=.'); // try deleteing
+  } catch (error) {}
+  return { endGet, endWright, avarage }; // return the ping data
 }
