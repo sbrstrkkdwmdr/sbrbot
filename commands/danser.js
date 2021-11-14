f636eadc15b7d17a8929e4ecebc666655ce69821
SaberStrike <sabersfear@gmail.com>
2021-11-15 09:04:28 +1100

    Update danser.js

--- commands/danser.js
+++ commands/danser.js
@@ -3,12 +3,12 @@ module.exports = {
     description: '',
     execute(message, args) {
         message.channel.send("https://discord.gg/UTPvbe8")
-        if(args[0] != help){
-
+      /*  if(args[0] != help){
+            
         }
         else{
             message.channel.send("ask here")
-        }
+        }*/
     }
 }
 //client.commands.get('').execute(message, args)
