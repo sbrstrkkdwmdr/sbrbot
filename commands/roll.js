320eb3de9b8a58c28bf0252b5ef1f6c56d62c070
SaberStrike <sabersfear@gmail.com>
2021-12-21 12:08:33 +1100

    currentdate in iso8601 for roll cmd

--- commands/roll.js
+++ commands/roll.js
@@ -8,7 +8,7 @@ module.exports = {
             if(w > 0){
                 let score = Math.floor(Math.random () * w + 1)
                 message.channel.send(` ${user} has rolled a(n) ${score} `)
-                console.log(`${currentDate}`)
+                console.log(`${currentDateISO} | ${currentDate}`)
                 console.log("command executed - roll")
                 let consoleloguserweeee = message.author
                 console.log(`requested by ${consoleloguserweeee.id} aka ${consoleloguserweeee.tag}`)
