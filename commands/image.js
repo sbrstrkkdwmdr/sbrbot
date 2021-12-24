b2de82e853ff583b549d929c7be3aa50049099b8
SaberStrike <sabersfear@gmail.com>
2021-12-25 10:59:18 +1100

    image command working (tutorial messed up the cx and key)

--- commands/image.js
+++ commands/image.js
@@ -13,16 +13,14 @@ module.exports = {
         // Note that ephemeral messages are only available with Interactions, so we can't make the response here as an ephemeral.
         // Meaning People can search dirty things and the image will be seen by everyone, It's up to you on how you can make this safe.
 
-        const GOOGLE_CX = "63d911bec99d0c63c"
-        const GOOGLE_KEY = "AIzaSyADArGVmH3N3uUcJ2AxfFxIY2fgLSbxk9g"
+        const cx = "63d911bec99d0c63c"
+        const key = "AIzaSyADArGVmH3N3uUcJ2AxfFxIY2fgLSbxk9g"
         
-        const cx = process.env.GOOGLE_CX // Watch the video to get your google cx.
-        const key = process.env.GOOGLE_KEY // Watch the video to get your google api key.
+        //const cx = process.env.GOOGLE_CX // Watch the video to get your google cx.
+        //const key = process.env.GOOGLE_KEY // Watch the video to get your google api key.
         let searchthing = args.splice(0,100).join(" ")
 
-        let res = await get(
-            `https://customsearch.googleapis.com/customsearch/v1?q=${searchthing}&cx=${cx}&key=${key}&searchType=image`
-        ).catch(e => console.log(e));
+        let res = await get(`https://customsearch.googleapis.com/customsearch/v1?q=${searchthing}&cx=${cx}&key=${key}&searchType=image`).catch(error => console.log(e));
         
         if (!res) return message.channel.send('Unable to fetch the requested image.');
         if (res.status >= 400) return message.channel.send(`Error ${res.status}: ${res.statusText}`);
