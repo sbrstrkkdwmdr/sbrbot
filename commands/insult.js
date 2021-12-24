657a98368328ac7bc6d8bd77b499ebd057479d91
SaberStrike <sabersfear@gmail.com>
2021-12-24 18:55:52 +1100

    change insult command to use an array

--- commands/insult.js
+++ commands/insult.js
@@ -8,12 +8,14 @@ module.exports = {
             let consoleloguserweeee = message.author
             console.log(`requested by ${consoleloguserweeee.id} aka ${consoleloguserweeee.tag}`)
             console.log("")
+            let rdm = ["キモイ", "you're a 10... on the pH scale cuz ya basic!", "sharthead", "dumbface", "insert insult here, I'll think of one later"];
+            let ins = rdm[Math.floor(Math.random() * rdm.length)];
             let user = message.mentions.users.first();
             if(!user){
                 message.channel.send("a user needs to be mentioned")
             }
             else{
-            message.channel.send(`${user}はキモいです。うんこ食べてくださいｗｗｗ`)
+            message.channel.send(`${user} ${ins}`)
             message.delete();}
             }
             else{
