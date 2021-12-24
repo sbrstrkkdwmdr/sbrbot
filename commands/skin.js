1aab8a6f7d4e8fcbc2fa84400e5b18cfad8c6d21
SaberStrike <sabersfear@gmail.com>
2021-12-24 17:59:58 +1100

    skins use numbers instead

--- commands/skin.js
+++ commands/skin.js
@@ -8,52 +8,52 @@ module.exports = {
         }
         else{        
             switch(skinname){
-            case 'custom':
+            case '1':
                 message.reply("https://sbrstrkkdwmdr.github.io/sbr-web/osu-skin/image/01custom/a")
                 break;
 
-            case 'customv2':
+            case '2':
                 message.reply("https://sbrstrkkdwmdr.github.io/sbr-web/osu-skin/image/02custom/a")
                 break;
 
-            case 'typex':
+            case '3':
                 message.reply(("https://sbrstrkkdwmdr.github.io/sbr-web/osu-skin/image/03x/a"))
                 break;
 
-            case 'typey':
+            case '4':
                 message.reply("https://sbrstrkkdwmdr.github.io/sbr-web/osu-skin/image/04y/a")
                 break;
 
-            case 'typez':
+            case '5':
                 message.reply("https://sbrstrkkdwmdr.github.io/sbr-web/osu-skin/image/05z/a")
                 break;
 
-            case '0':
+            case '6':
                 message.reply("https://sbrstrkkdwmdr.github.io/sbr-web/osu-skin/image/06ss0/a")
                 break;
             
-            case '1':
+            case '7':
                 message.reply("https://sbrstrkkdwmdr.github.io/sbr-web/osu-skin/image/07ss1/a")
                 break;
         
-            case 'sbr':
+            case '8':
                 message.reply("https://sbrstrkkdwmdr.github.io/sbr-web/osu-skin/image/08sbr/a")
                 break;
 
-            case 'prjct_sbr':
+            case '9':
                 message.reply("https://sbrstrkkdwmdr.github.io/sbr-web/osu-skin/image/09prjct/a")
                 break;
             
-            case 'sbr_ud':
+            case '10':
                 message.reply("https://sbrstrkkdwmdr.github.io/sbr-web/osu-skin/image/10ud/a")
                 break;
 
             default:
-            message.channel.send("skins - \n`custom\ncustomv2\ntypex\ntypey\ntypez\n0\n1\nsbr\nprjct_sbr\nsbr_ud`")
+            message.channel.send("***SKINS*** \n1 - custom\n2 - customv2\n3 - Type X\n4 - Type Y \n5 - Type Z\n6 - SaberStrike 0\n7 - SaberStrike 1\n8 - sbr\n9 - prjct sbr\n10 - SBR UnDefined")
 
         }}
         console.log(`${currentDateISO} | ${currentDate}`)
-        console.log("command executed - ")
+        console.log("command executed - skin")
         let consoleloguserweeee = message.author
         console.log(`requested by ${consoleloguserweeee.id} aka ${consoleloguserweeee.tag}`)
         console.log("")   
