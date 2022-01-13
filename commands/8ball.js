527da48d8f56df3c8380dd0ae4519755b0990120
SaberStrike <sabersfear@gmail.com>
2022-01-13 14:54:57 +1100

    8ball cant crash now!

--- commands/8ball.js
+++ commands/8ball.js
@@ -3,7 +3,7 @@ module.exports = {
     description: '',
     execute(message, args, currentDate, currentDateISO) {
         let rdm = ["yes", "no clue.", "知らない", "nope", "yeah", "definitely maybe not", "nah", "yeah of course", "絶対!!!", "多分", 
-        "i didn't quite catch that, ask again?", "ehhhhhhh", ""];
+        "i didn't quite catch that, ask again?", "ehhhhhhh", "⠀"];
         let ball = rdm[Math.floor(Math.random() * rdm.length)];
         message.reply(String(`${ball}`))   
         console.log(`${currentDateISO} | ${currentDate}`)
