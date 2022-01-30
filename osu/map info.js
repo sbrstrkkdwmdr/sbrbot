77fd8b7c7eb1f953e0bfa97e5628525ef216bb39
SaberStrike <sabersfear@gmail.com>
2022-01-30 20:37:12 +1100

    a

--- osu/map info.js
+++
@@ -1,8 +0,0 @@
-/*like this
-name [diff] //mapper
-graph
-
-pp if SS
-pp if 95
-pp if 90
-*/
--- osu/top.js
+++
@@ -1,9 +0,0 @@
-/* show top 10 plays
-show like
-player name         profile pc
-silver ss gold ss etc.
-glob rank, country rank, peak (if doable), total pp
-rank graph (if possible)
-plays in order
-map, pp, combo, hitresult, ranking grade
-*/
