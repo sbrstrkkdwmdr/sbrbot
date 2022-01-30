9f2057630628a9b81a53bba3e5b7f97c7960f80e
SaberStrike <sabersfear@gmail.com>
2022-01-30 20:37:35 +1100

    testing

---
+++ commands/auto.js
@@ -0,0 +1,17 @@
+module.exports = {
+    name: 'auto',
+    description: '',
+    execute(message, args, currentDate, currentDateISO) {
+        if(message.author.id == '503794887318044675'){
+            message.reply("success")
+        console.log(`${currentDateISO} | ${currentDate}`)
+        console.log("command executed - auto")
+        let consoleloguserweeee = message.author
+        console.log(`requested by ${consoleloguserweeee.id} aka ${consoleloguserweeee.tag}`)
+        console.log("")}
+        else {
+            message.reply("skill issue")
+        }
+    }
+}
+//client.commands.get('').execute(message, args)
