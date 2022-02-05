4ab333e04cad229e5ebbc55bc19bbd661713a6d7
SaberStrike <sabersfear@gmail.com>
2022-02-05 11:35:23 +1100

    testign

---
+++ testing/log.js
@@ -0,0 +1,15 @@
+console.log('w')
+console.error('ee')
+console.time()
+console.timeLog()
+console.timeStamp()
+console.timeEnd()
+console.warn('hey')
+for (var i=0; i<5; i++) {
+    console.log("Hello, %s. You've called me %d times.", "Bob", i+1);
+  }
+  
+console.group("1")
+console.log("x1")
+console.group("3")
+console.log("x3")
