ec8ac0c1590a3712aaf6c51191e243df55c1a318
SaberStrike <sabersfear@gmail.com>
2022-02-11 18:26:20 +1100

    replayparse

--- links/replayparse.js
+++ links/replayparse.js
@@ -20,6 +20,7 @@ module.exports = {
         const replay = osuReplayParser.parseReplay(replayPath);
         fs.writeFileSync("replay.json", JSON.stringify(replay, null, 2))
 
+        let maphash = JSON.stringify(replay, ['beatmapMD5']).replaceAll('{', '').replaceAll('"', '').replaceAll('}', '').replaceAll(':', '').replaceAll('beatmapMD5', '');
         let playername = JSON.stringify(replay, ['playerName']).replaceAll('{', '').replaceAll('"', '').replaceAll('}', '').replaceAll(':', '').replaceAll('playerName', '');
         let timeset = JSON.stringify(replay, ['timestamp']).replaceAll('{', '').replaceAll('"', '').replaceAll('}', '').replaceAll(':', '').replaceAll('timestamp', '');
         let maxcombo = JSON.stringify(replay, ['max_combo']).replaceAll('{', '').replaceAll('"', '').replaceAll('}', '').replaceAll(':', '').replaceAll('max_combo', '');
@@ -27,15 +28,11 @@ module.exports = {
         let hit100s = JSON.stringify(replay, ['number_100s']).replaceAll('{', '').replaceAll('"', '').replaceAll('}', '').replaceAll(':', '').replaceAll('number_100s', '');
         let hit50s = JSON.stringify(replay, ['number_50s']).replaceAll('{', '').replaceAll('"', '').replaceAll('}', '').replaceAll(':', '').replaceAll('number_50s', '');
         let misses = JSON.stringify(replay, ['misses']).replaceAll('{', '').replaceAll('"', '').replaceAll('}', '').replaceAll(':', '').replaceAll('misses', '');
-        
-        let Embed = new Discord.MessageEmbed()
-        .setColor(0x462B71)
-        .setTitle("Information for replay")
-        //.setImage(mapbg)
-        .setDescription(`***${playername}***\nScore set on ${timeset}\n${hit300s}/${hit100s}/${hit50s}/${misses}\nCombo:***${maxcombo}***`);
-        message.reply({embeds: [Embed]})
-        //console.log(replay);
-        /*try{
+        let hitkatu = JSON.stringify(replay, ['katus']).replaceAll('{', '').replaceAll('"', '').replaceAll('}', '').replaceAll(':', '').replaceAll('katus', '');
+        let hitgeki = JSON.stringify(replay, ['gekis']).replaceAll('{', '').replaceAll('"', '').replaceAll('}', '').replaceAll(':', '').replaceAll('gekis', '');
+        let mods = JSON.stringify(replay, ['mods']).replaceAll('{', '').replaceAll('"', '').replaceAll('}', '').replaceAll(':', '').replaceAll('mods', '');
+
+        try{
             let oauthurl = new URL ("https://osu.ppy.sh/oauth/token");
             let body1 = {
                 "client_id": osuclientid,
@@ -51,52 +48,88 @@ module.exports = {
             .then(res => res.json())
             .then(output => fs.writeFileSync("osuauth.json", JSON.stringify(output, null, 2)))
             ;
-            const mapurl = `https://osu.ppy.sh/api/v2/beatmaps/${pickeduserX}`;
+            console.log("writing data to osuauth.json")
+            console.log("")
             
-            fetch(mapurl, {
-                method: "GET",
+            const userinfourl = `https://osu.ppy.sh/api/v2/users/${playername}/osu`;
+            
+            fetch(userinfourl, {
                 headers: {
-                    "Authorization": `Bearer ${access_token}`,
-                    "Content-Type": "application/json",
-                    "Accept": "application/json",
+                    Authorization: `Bearer ${access_token}`
                 }
             }).then(res => res.json())
             .then(output2 => 
                 {
-					const mapdata = output2;
-					//let mapdataP2 = JSON.stringify("[\n" + mapdataP1 + "\n]");
-					//const mapdata = JSON.stringify("[\n" + mapdataP1 + "\n]");
-                fs.writeFileSync("map.json", JSON.stringify(mapdata, null, 2))
-            try{
-            fs.writeFile(fileName, JSON.stringify(file, null, 2), function writeJSON(err) {
-                if (err) return console.log(err);
-                console.log(JSON.stringify(file));
-                console.log('writing to ' + fileName);
-              });
+                try{const osudata = output2;
+                fs.writeFileSync("osu.json", JSON.stringify(osudata, null, 2));
+                console.log("writing data to osu.json")
+                console.log("")
+                console.groupEnd() 
+            const mapurl = `https://osu.ppy.sh/api/v2/beatmaps/lookup?h=${maphash}`
+(async () => {        
+        const score = {
+            beatmap_id: 2134,
+            score: '6795149',
+            maxcombo: '630',
+            count50: hit50s,
+            count100: hit100s,
+            count300: hit300s,
+            countmiss: '0',
+            countkatu: hitkatu,
+            countgeki: hitgeki,
+            perfect: '0',
+            enabled_mods: '64',
+            user_id: rsplayerid,
+            date: timeset,
+            rank: 'S',
+            score_id: '4057765057'
+          }
+          const scorenofc = {
+            beatmap_id: rsmapid,
+            score: '6795149',
+            maxcombo: '630',
+            count50: hit50s,
+            count100: hit100s,
+            count300: hit300s,
+            countmiss: misses,
+            countkatu: hitkatu,
+            countgeki: hitgeki,
+            perfect: '0',
+            enabled_mods: '64',
+            user_id: rsplayerid,
+            date: timeset,
+            rank: 'S',
+            score_id: '4057765057'
+          }
+          fs.writeFileSync("rsppcalc.json", JSON.stringify(score, null, 2));
+                  let ppfc = new std_ppv2().setPerformance(score);
+                  let pp =  new std_ppv2().setPerformance(scorenofc);
+                  if(mods){
+                    pp = new std_ppv2().setPerformance(scorenofc).setMods(`${mods}`)
+                    ppfc = new std_ppv2().setPerformance(score).setMods(`${mods}`);
+                }
+                if(!mods){
+                    pp = new std_ppv2().setPerformance(scorenofc).setMods('NM')
+                    ppfc = new std_ppv2().setPerformance(score).setMods('NM');
+                }
 
-            let Embed = new Discord.MessageEmbed()
-            .setColor(0x462B71)
-            .setTitle("Information for " + maptitle)
-            .setImage(mapbg)
-            .setDescription(``);
-            message.reply({ embeds: [Embed]})
-            
-    } catch(error){
-				message.reply("error - map not found")
-				console.log(error)
-				console.log("")
-			}
-			
-			
-		
-            });
-        } catch(error){
+        let Embed = new Discord.MessageEmbed()
+        .setColor(0x462B71)
+        .setTitle("Information for replay (HEAVILY WIP)")
+        //.setImage(mapbg)
+        .setDescription(`***${playername}***\nScore set on ${timeset}\n${hit300s}/${hit100s}/${hit50s}/${misses}\nCombo:***${maxcombo}***`);
+        message.reply({embeds: [Embed]})
+})();
+    }       catch(error){
+            message.channel.send("Error - 1")
             console.log(error)
-            message.channel.send("Error - 2")
-        }*/
-    } 
-        
-        catch(error){
+        }
+            })
+        }        catch(error){
+            message.channel.send("Error - 1")
+            console.log(error)
+        }
+        }   catch(error){
             message.channel.send("Error - 1")
             console.log(error)
         }
