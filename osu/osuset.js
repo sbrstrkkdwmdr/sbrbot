deea61aba43e7a43ef093251f0691fe008aaacd1
SaberStrike <sabersfear@gmail.com>
2022-03-22 18:31:26 +1100

    added mode option to osuset

--- osu/osuset.js
+++ osu/osuset.js
@@ -10,13 +10,16 @@ module.exports = {
         console.log(`requested by ${consoleloguserweeee.id} aka ${consoleloguserweeee.tag}`)
         console.log("") 
         //interaction.reply('...')
-
+        let type = options.getString('type')
+        console.log(type + '|')
+        if(!type || type == 'username' || type == 'name' || type == 'user' || type == null || type == 'null'){
 		try {
 			// equivalent to: INSERT INTO tags (name, description, username) values (?, ?, ?);
 			await userdatatags.create({
 				name: interaction.member.user.id,
 				description: pickeduserX,
 				username: interaction.member.user.id,
+                mode: 'osu'
 			});
 
 			interaction.reply(`${pickeduserX} added.`)
@@ -33,6 +36,34 @@ module.exports = {
 			}
 
 			return interaction.reply('Something went wrong with adding a username.');
-		}
+		}}
+        else if(type == 'mode'){
+            try {
+                // equivalent to: INSERT INTO tags (name, description, username) values (?, ?, ?);
+                await userdatatags.create({
+                    name: interaction.member.user.id,
+                    username: interaction.member.user.id,
+                    mode: pickeduserX
+                });
+    
+                interaction.reply(`${pickeduserX} added.`)
+                console.log("sent")
+            }
+            catch (error) {
+                //console.log(error)
+                if (error.name === 'SequelizeUniqueConstraintError') {
+                    affectedRows = await userdatatags.update({ mode: pickeduserX }, { where: { name: interaction.member.user.id } });
+            
+                if (affectedRows > 0) {
+                    return interaction.reply(`mode updated.`);
+                }
+                    //return interaction.reply('That username is already taken.');
+                }
+                
+                return interaction.reply('Something went wrong with editing mode.');
+            }}
+        else{
+            //return interaction.channel.send("error - can only update mode or username")
+        }
     }
 }
