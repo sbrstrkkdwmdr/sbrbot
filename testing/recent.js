24a7cb7927bc3d16bb0abcfc0d4abf9235589900
SaberStrike <sabersfear@gmail.com>
2022-04-12 21:44:48 +1000

    Update recent.js

--- testing/recent.js
+++ testing/recent.js
@@ -1,10 +1,34 @@
-/*show like dis
-player - profile
-rank achieved
-pp, combo other score info, pp if FC etc.
+const fetch = require('node-fetch');
+const POST = require('node-fetch');
+const fs = require('fs');
+const { std_ppv2, taiko_ppv2, catch_ppv2, mania_ppv2 } = require('booba');
+const { calculateStarRating } = require('osu-sr-calculator')
+const { osulogdir } = require('../logconfig.json')
+            console.log('pp');
 
--map info-
-name [diff] //mapper
-graph
-SS pp
- */
+              (async () => {
+                const score = {
+                    beatmap_id: '2860521',
+                    score: '7573665',
+                    maxcombo: '551',
+                    count50: '0',
+                    count100: '0',
+                    count300: '383',
+                    countmiss: '0',
+                    countkatu: '0',
+                    countgeki: '0',
+                    perfect: '0',
+                    enabled_mods: '72',
+                    user_id: '4882979',
+                    date: '2022-03-15 20:51:54',
+                    rank: 'SS',
+                    score_id: '3533591671'
+                  }
+
+    let pp = new std_ppv2().setPerformance(score);
+        let ppSSjson = await pp.compute();
+
+                console.log(ppSSjson)
+            })();
+
+//client.commands.get('').execute(message, args)
