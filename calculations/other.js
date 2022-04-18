70558be1db0b9c2acf9dc6e0739c140fbff21b8b
SaberStrike <sabersfear@gmail.com>
2022-04-18 12:56:53 +1000

    Create other.js

---
+++ calculations/other.js
@@ -0,0 +1,40 @@
+function findHCF(x, y) {
+    if(isNaN(x, y)) return NaN;
+                    
+    while (Math.max(x, y) % Math.min(x, y) != 0) {
+       if (x > y) {
+          x %= y;
+       }
+       else {
+          y %= x;
+       }
+    }
+    return Math.min(x, y);
+}
+function findLCM (n1, n2) {
+    if(isNaN(n1, n2)) return NaN;
+    let lar = Math.max(n1, n2);
+    let small = Math.min(n1, n2);
+    
+    
+    let i = lar;
+    while(i % small !== 0){
+      i += lar;
+    }
+    
+    
+    return i;
+  }
+function pythag (a,b){
+    if(isNaN(a,b)) return NaN;
+    let cp = (a**2)+(b**2);
+    c = Math.sqrt(cp)
+    return(c)
+}
+function sigfig (a){
+    if(isNaN(a)) return NaN;
+    let s = a/(10**(a.toString().length - 1))
+    let b = s + ' x 10^' + (a.toString().length - 1)
+    return b;
+}
+module.exports = { findHCF, findLCM, pythag, sigfig }
