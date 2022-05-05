4d974fc3043b9679b27d42190c8e4865f8ff2c3a
SaberStrike <71379681+sbrstrkkdwmdr@users.noreply.github.com>
2022-05-06 09:08:54 +1000

    a

--- osu11/osubest.js
+++ osu11/osubest.js
@@ -14,82 +14,84 @@ module.exports = {
     description: '',
     execute(message, args, Discord, currentDate, currentDateISO) {
         fs.appendFileSync(osulogdir, "\n" + '--- COMMAND EXECUTION ---')
-//        `https://osutrack-api.ameo.dev/hiscores?user=${pickeduserX}&mode=0`
+        //        `https://osutrack-api.ameo.dev/hiscores?user=${pickeduserX}&mode=0`
         let url = `https://osutrack-api.ameo.dev/bestplays?mode=0`;
-       /* const hiscores = [
-            {
-              "beatmap_id": 49977,
-              "score": 57002,
-              "pp": 1.87718,
-              "mods": 0,
-              "rank": "C",
-              "score_time": "2011-01-09T02:21:38.000Z",
-              "update_time": "2020-03-13T08:44:01.000Z"
-            },
-            {
-              "beatmap_id": 11515,
-              "score": 78210,
-              "pp": 1.01566,
-              "mods": 0,
-              "rank": "C",
-              "score_time": "2011-01-09T13:54:35.000Z",
-              "update_time": "2020-03-13T08:44:01.000Z"
-            },
-            {
-              "beatmap_id": 41227,
-              "score": 31416,
-              "pp": 0.357312,
-              "mods": 0,
-              "rank": "D",
-              "score_time": "2011-01-09T13:56:51.000Z",
-              "update_time": "2020-03-13T08:44:01.000Z"
-            },
-            ];*/
-    try {fetch(url)
-    .then(res => res.json())
-    .then(out =>
-{        out.sort((a, b) => b.pp - a.pp);
-    const topHiscores = out.slice(0, 5);
-        fs.writeFileSync("debug/osubest.json", JSON.stringify(topHiscores, null, 2));
-        fs.appendFileSync(osulogdir, "\n" + "writing data to w.json")
-        fs.appendFileSync(osulogdir, "\n" + "")
-        let Embedpenis = new Discord.MessageEmbed()
-        .setColor(0xFFC1EC)
-        .setTitle("Top plays of all time")
-        .setImage(``)
-        for(i=0;i<5;i++){
-            let bmid = topHiscores[i].beatmap_id
-            let sc = topHiscores[i].score 
-            let perform = topHiscores.pp 
-            let modify = topHiscores.mods
-            if(modify){
-                mods = modify
-            } else {
-                mods = ''
+        /* const hiscores = [
+             {
+               "beatmap_id": 49977,
+               "score": 57002,
+               "pp": 1.87718,
+               "mods": 0,
+               "rank": "C",
+               "score_time": "2011-01-09T02:21:38.000Z",
+               "update_time": "2020-03-13T08:44:01.000Z"
+             },
+             {
+               "beatmap_id": 11515,
+               "score": 78210,
+               "pp": 1.01566,
+               "mods": 0,
+               "rank": "C",
+               "score_time": "2011-01-09T13:54:35.000Z",
+               "update_time": "2020-03-13T08:44:01.000Z"
+             },
+             {
+               "beatmap_id": 41227,
+               "score": 31416,
+               "pp": 0.357312,
+               "mods": 0,
+               "rank": "D",
+               "score_time": "2011-01-09T13:56:51.000Z",
+               "update_time": "2020-03-13T08:44:01.000Z"
+             },
+             ];*/
+        try {
+            fetch(url)
+            .then(res => res.json())
+            .then(out => {
+                out.sort((a, b) => b.pp - a.pp);
+                const topHiscores = out.slice(0, 5);
+                fs.writeFileSync("debug/osubest.json", JSON.stringify(topHiscores, null, 2));
+                fs.appendFileSync(osulogdir, "\n" + "writing data to w.json")
+                fs.appendFileSync(osulogdir, "\n" + "")
+                let Embedpenis = new Discord.MessageEmbed()
+                    .setColor(0xFFC1EC)
+                    .setTitle("Top plays of all time")
+                    .setImage(``)
+                for (i = 0; i < 5; i++) {
+                    let bmid = topHiscores[i].beatmap_id
+                    let sc = topHiscores[i].score
+                    let perform = topHiscores.pp
+                    let modify = topHiscores.mods
+                    if (modify) {
+                        mods = modify
+                    } else {
+                        mods = ''
+                    }
+                    let grade = topHiscores.rank
+                    let sctime = topHiscores.score_time.toString().slice(0, 10)
+                    let user = topHiscores.user
+                        .addField(`#${i + 1} | Score set on ${sctime} by https://osu.ppy.sh/u/${user}`, `https://osu.ppy.sh/b/${bmid} \n**SCORE**: ${sc} | ${perform}**pp**\n+${mods} | ${grade}`, false)
+                }
+
+                message.reply({ embeds: [Embedpenis] });
+                //message.reply("```json\nTOP SCORES FOR " + pickeduserX + "\n" + JSON.stringify(topHiscores, null, 2).replaceAll('"', '').replaceAll('beatmap_id', 'beatmap id').replaceAll('[', '').replaceAll(']', '').replaceAll(',', '').replaceAll('}', '').replaceAll('{', '------------') + "```");
             }
-            let grade = topHiscores.rank 
-            let sctime = topHiscores.score_time.toString().slice(0, 10)
-            let user = topHiscores.user 
-            .addField(`#${i+1} | Score set on ${sctime} by https://osu.ppy.sh/u/${user}`, `https://osu.ppy.sh/b/${bmid} \n**SCORE**: ${sc} | ${perform}**pp**\n+${mods} | ${grade}`, false)
+            )
+        } catch (error) {
+            message.reply(error)
+            fs.appendFileSync(osulogdir, "\n" + error)
+            fs.appendFileSync(osulogdir, "\n" + getStackTrace(error))
         }
-            
-            message.reply({ embeds: [Embedpenis] });
-        //message.reply("```json\nTOP SCORES FOR " + pickeduserX + "\n" + JSON.stringify(topHiscores, null, 2).replaceAll('"', '').replaceAll('beatmap_id', 'beatmap id').replaceAll('[', '').replaceAll(']', '').replaceAll(',', '').replaceAll('}', '').replaceAll('{', '------------') + "```");
-    }
-    )} catch (error) {
-        message.reply(error)
-        fs.appendFileSync(osulogdir, "\n" + error)
-        fs.appendFileSync(osulogdir, "\n" + getStackTrace(error))
-    }
-    
+
 
         fs.appendFileSync(osulogdir, "\n" + `${currentDateISO} | ${currentDate}`)
         fs.appendFileSync(osulogdir, "\n" + "command executed - osubest")
         let consoleloguserweeee = message.author
         fs.appendFileSync(osulogdir, "\n" + `requested by ${consoleloguserweeee.id} aka ${consoleloguserweeee.tag}`)
-        fs.appendFileSync(osulogdir, "\n" + "") 
+        fs.appendFileSync(osulogdir, "\n" + "")
         console.groupEnd()
-        
+
     }
 }
 //client.commands.get('').execute(message, args)
--- osu11/osubestrs.js
+++ osu11/osubestrs.js
@@ -14,88 +14,90 @@ module.exports = {
     description: '',
     execute(message, args, Discord, currentDate, currentDateISO, curdateyesterday, curdatetmr, curtimezone) {
         fs.appendFileSync(osulogdir, "\n" + '--- COMMAND EXECUTION ---')
-//        `https://osutrack-api.ameo.dev/hiscores?user=${pickeduserX}&mode=0`
+        //        `https://osutrack-api.ameo.dev/hiscores?user=${pickeduserX}&mode=0`
         let url = `https://osutrack-api.ameo.dev/bestplays?mode=0&from=${curdateyesterday}&to=${curdatetmr}`;
-       /* const hiscores = [
-            {
-              "beatmap_id": 49977,
-              "score": 57002,
-              "pp": 1.87718,
-              "mods": 0,
-              "rank": "C",
-              "score_time": "2011-01-09T02:21:38.000Z",
-              "update_time": "2020-03-13T08:44:01.000Z"
-            },
-            {
-              "beatmap_id": 11515,
-              "score": 78210,
-              "pp": 1.01566,
-              "mods": 0,
-              "rank": "C",
-              "score_time": "2011-01-09T13:54:35.000Z",
-              "update_time": "2020-03-13T08:44:01.000Z"
-            },
-            {
-              "beatmap_id": 41227,
-              "score": 31416,
-              "pp": 0.357312,
-              "mods": 0,
-              "rank": "D",
-              "score_time": "2011-01-09T13:56:51.000Z",
-              "update_time": "2020-03-13T08:44:01.000Z"
-            },
-            ];*/
-    try {fetch(url)
-    .then(res => res.json())
-    .then(out =>
-{        out.sort((a, b) => b.pp - a.pp);
-    const topHiscores = out.slice(0, 10);
-        fs.writeFileSync("debug/osubest.json", JSON.stringify(topHiscores, null, 2));
+        /* const hiscores = [
+             {
+               "beatmap_id": 49977,
+               "score": 57002,
+               "pp": 1.87718,
+               "mods": 0,
+               "rank": "C",
+               "score_time": "2011-01-09T02:21:38.000Z",
+               "update_time": "2020-03-13T08:44:01.000Z"
+             },
+             {
+               "beatmap_id": 11515,
+               "score": 78210,
+               "pp": 1.01566,
+               "mods": 0,
+               "rank": "C",
+               "score_time": "2011-01-09T13:54:35.000Z",
+               "update_time": "2020-03-13T08:44:01.000Z"
+             },
+             {
+               "beatmap_id": 41227,
+               "score": 31416,
+               "pp": 0.357312,
+               "mods": 0,
+               "rank": "D",
+               "score_time": "2011-01-09T13:56:51.000Z",
+               "update_time": "2020-03-13T08:44:01.000Z"
+             },
+             ];*/
+        try {
+            fetch(url)
+            .then(res => res.json())
+            .then(out => {
+                out.sort((a, b) => b.pp - a.pp);
+                const topHiscores = out.slice(0, 10);
+                fs.writeFileSync("debug/osubest.json", JSON.stringify(topHiscores, null, 2));
 
-        let Embeda = new Discord.MessageEmbed()
-            .setColor(0xFFC1EC)
-            .setTitle("Top plays from the past 24h")
-            .setImage(``)
-            for(i=0;i<10;i++){
-                let bmid = topHiscores[i].beatmap_id
-                let sc = topHiscores[i].score 
-                let perform = topHiscores.pp 
-                let modify = topHiscores.mods
-                if(modify){
-                    mods = modify
-                } else {
-                    mods = ''
+                let Embeda = new Discord.MessageEmbed()
+                    .setColor(0xFFC1EC)
+                    .setTitle("Top plays from the past 24h")
+                    .setImage(``)
+                for (i = 0; i < 10; i++) {
+                    let bmid = topHiscores[i].beatmap_id
+                    let sc = topHiscores[i].score
+                    let perform = topHiscores.pp
+                    let modify = topHiscores.mods
+                    if (modify) {
+                        mods = modify
+                    } else {
+                        mods = ''
+                    }
+                    let grade = topHiscores.rank
+                    let sctime = topHiscores.score_time.toString().slice(0, 10)
+                    let user = topHiscores.user
+                        .addField(`#${i + 1} | Score set on ${sctime} by https://osu.ppy.sh/u/${user}`, `https://osu.ppy.sh/b/${bmid} \n**SCORE**: ${sc} | ${perform}**pp**\n+${mods} | ${grade}`, false)
                 }
-                let grade = topHiscores.rank 
-                let sctime = topHiscores.score_time.toString().slice(0, 10)
-                let user = topHiscores.user 
-                .addField(`#${i+1} | Score set on ${sctime} by https://osu.ppy.sh/u/${user}`, `https://osu.ppy.sh/b/${bmid} \n**SCORE**: ${sc} | ${perform}**pp**\n+${mods} | ${grade}`, false)
+
+
+
+                /*let EmbedScores = new Discord.MessageEmbed()
+                   .setColor(0x462B75)
+                   .setTitle("The top 10 highest pp scores over the past 24h: from " + curdateyesterday + " to " + curdatetmr + ` | ${curtimezone}`)
+                   .setDescription(" ")
+                   .addField(`https://osu.ppy.sh/b/${bmid1} \nScore set on ${sctime1} by https://osu.ppy.sh/u/${user1} \n **SCORE**: ${sc1} | ${perform1} **PP**\n + ${modify1} | ${grade1}`)*/
+                message.reply({ content: `The top 10 highest pp scores over the past 24h: from ${curdateyesterday} to ${curdatetmr} | ${curtimezone}`, embeds: [Embeda] })
+                //message.reply("```json\nTOP SCORES FOR " + pickeduserX + "\n" + JSON.stringify(topHiscores, null, 2).replaceAll('"', '').replaceAll('beatmap_id', 'beatmap id').replaceAll('[', '').replaceAll(']', '').replaceAll(',', '').replaceAll('}', '').replaceAll('{', '------------') + "```");
             }
-        
-    
-       
-         /*let EmbedScores = new Discord.MessageEmbed()
-            .setColor(0x462B75)
-            .setTitle("The top 10 highest pp scores over the past 24h: from " + curdateyesterday + " to " + curdatetmr + ` | ${curtimezone}`)
-            .setDescription(" ")
-            .addField(`https://osu.ppy.sh/b/${bmid1} \nScore set on ${sctime1} by https://osu.ppy.sh/u/${user1} \n **SCORE**: ${sc1} | ${perform1} **PP**\n + ${modify1} | ${grade1}`)*/
-            message.reply({content: `The top 10 highest pp scores over the past 24h: from ${curdateyesterday} to ${curdatetmr} | ${curtimezone}`, embeds: [Embeda]})
-            //message.reply("```json\nTOP SCORES FOR " + pickeduserX + "\n" + JSON.stringify(topHiscores, null, 2).replaceAll('"', '').replaceAll('beatmap_id', 'beatmap id').replaceAll('[', '').replaceAll(']', '').replaceAll(',', '').replaceAll('}', '').replaceAll('{', '------------') + "```");
-    }
-    )} catch (error) {
-        fs.appendFileSync(osulogdir, "\n" + error)
-        fs.appendFileSync(osulogdir, "\n" + getStackTrace(error))
-        console.groupEnd()
-    }
-    
+            )
+        } catch (error) {
+            fs.appendFileSync(osulogdir, "\n" + error)
+            fs.appendFileSync(osulogdir, "\n" + getStackTrace(error))
+            console.groupEnd()
+        }
+
 
         fs.appendFileSync(osulogdir, "\n" + `${currentDateISO} | ${currentDate}`)
         fs.appendFileSync(osulogdir, "\n" + "command executed - osubest past 24h")
         let consoleloguserweeee = message.author
         fs.appendFileSync(osulogdir, "\n" + `requested by ${consoleloguserweeee.id} aka ${consoleloguserweeee.tag}`)
-        fs.appendFileSync(osulogdir, "\n" + "") 
+        fs.appendFileSync(osulogdir, "\n" + "")
         console.groupEnd()
-        
+
     }
 }
 //client.commands.get('').execute(message, args)
