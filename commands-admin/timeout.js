4f24f855581b053156dec8958e8e01d4c136c055
SaberStrike <71379681+sbrstrkkdwmdr@users.noreply.github.com>
2022-05-25 10:01:48 +1000

    Update timeout.js

--- commands-admin/timeout.js
+++ commands-admin/timeout.js
@@ -1,21 +1,32 @@
+const fs = require('fs')
+const { adminlogdir } = require('../logconfig.json')
 module.exports = {
     name: 'timeout',
     description: 'timeout',
-    execute(message, args, client, Discord, currentDate, currentDateISO){
+    execute(message, args, client, Discord, currentDate, currentDateISO) {
         let user = message.mentions.members.first()
-        let time = args.splice(2,2).join("")
+        let time = args.splice(2, 2).join("")
         let reason = args.splice(3, 100).join(" ")
-        if(!reason){
+        if (!reason) {
             reasonx = 'undefined reason'
         } else {
             reasonx = reason
         }
-        if(isNaN(time)){
+        if (isNaN(time)) {
             time = 1000 * 60 * 60 * 24 //ms -> s -> m -> h -> d
         }
-        if(user){
-            member = message.guild.members.cache.get(user.id)
-            member.timeout(time, reasonx)
+        if (user) {
+            try {
+                member = message.guild.members.cache.get(user.id)
+                member.timeout(time, reasonx)
+                fs.appendFileSync(adminlogdir, "\n" + `timed out ${user} AKA ${user.id} for ${reaswon}`)
+                fs.appendFileSync(adminlogdir, "\n" + "")
+            }
+            catch (error) {
+                fs.appendFileSync(adminlogdir, "\n" + `cannot timeout ${user} AKA  ${user.id}`)
+                fs.appendFileSync(adminlogdir, "\n" + error)
+                fs.appendFileSync(adminlogdir, "\n" + "")
+            }
         }
     }
 }
