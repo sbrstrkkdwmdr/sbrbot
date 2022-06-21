e6d791af090da72fed6b0128b4f8f470c0d6db3b
SaberStrike <sabersfear@gmail.com>
2022-06-21 20:24:43 +1000

    added exec function to commandchecks

--- configs/commandchecks.js
+++ configs/commandchecks.js
@@ -15,4 +15,8 @@ function isOwner(userid){
     }
     return false
 }
-module.exports = { isOwner }
+function exec(cmd, handler = function (error, stdout, stderr) { console.log(stdout); if (error !== null) { console.log(stderr) } }) {
+    const childfork = require('child_process');
+    return childfork.exec(cmd, handler);
+}
+module.exports = { isOwner, exec }
