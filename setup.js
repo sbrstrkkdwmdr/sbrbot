d22006e58ffd0bfea44f1d4a6d5ab67e3b0319e2
SaberStrike <sabersfear@gmail.com>
2022-06-21 21:05:43 +1000

    added replay.osr to setup

--- setup.js
+++ setup.js
@@ -12,3 +12,4 @@ let config =
 }
 fs.writeFileSync('./configs/config.json', JSON.stringify(config, null, 2), 'utf-8')
 fs.writeFileSync('./configs/prevmap.json', JSON.stringify(({ id: 32345 }), null, 2), 'utf-8');
+fs.writeFileSync('./files/replay.osr', '', 'utf-8');
