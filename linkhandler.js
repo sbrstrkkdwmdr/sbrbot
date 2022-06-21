d98ba0b560f1e6b5e44028de05ff09249d99bf7e
SaberStrike <sabersfear@gmail.com>
2022-06-21 21:06:08 +1000

    finished rewrite for replayparse

--- linkhandler.js
+++ linkhandler.js
@@ -1,23 +1,33 @@
-module.exports =(userdata, client, Discord, osuApiKey, osuClientID, osuClientSecret, config) => {
+const fs = require('fs')
+const https = require('https')
+module.exports = (userdata, client, Discord, osuApiKey, osuClientID, osuClientSecret, config) => {
     client.on('messageCreate', async (message) => {
         let currentDate = new Date();
         let currentDateISO = new Date().toISOString();
 
 
         let messagenohttp = message.content.replace('https://', '').replace('http://', '').replace('www.', '')
-        if(messagenohttp.startsWith('osu.ppy.sh/b/') || messagenohttp.startsWith('osu.ppy.sh/beatmaps/') || messagenohttp.startsWith('osu.ppy.sh/beatmapsets/') || messagenohttp.startsWith('osu.ppy.sh/s/')) {
+        if (messagenohttp.startsWith('osu.ppy.sh/b/') || messagenohttp.startsWith('osu.ppy.sh/beatmaps/') || messagenohttp.startsWith('osu.ppy.sh/beatmapsets/') || messagenohttp.startsWith('osu.ppy.sh/s/')) {
             client.links.get('osumaplink').execute(message, userdata, Discord, osuApiKey, osuClientID, osuClientSecret, config, currentDate, currentDateISO);
         }
-        if(messagenohttp.startsWith('osu.ppy.sh/u/') || messagenohttp.startsWith('osu.ppy.sh/users/')) {
+        if (messagenohttp.startsWith('osu.ppy.sh/u/') || messagenohttp.startsWith('osu.ppy.sh/users/')) {
             client.links.get('osuuserlink').execute(message, userdata, Discord, osuApiKey, osuClientID, osuClientSecret, config, currentDate, currentDateISO);
         }
-        
-        if(message.attachments.size > 0 && message.attachments.every(attachment => attachment.url.endsWith('.osr'))) {
-            client.links.get('replayparse').execute(message, userdata, Discord, osuApiKey, osuClientID, osuClientSecret, config, currentDate, currentDateISO);
-        }
-        if(messagenohttp.startsWith('osu.ppy.sh/scores/')) {
-            client.links.get('scoreparse').execute(message, userdata, Discord, osuApiKey, osuClientID, osuClientSecret, config, currentDate, currentDateISO);
 
+        if (message.attachments.size > 0 && message.attachments.every(attachment => attachment.url.endsWith('.osr'))) {
+            attachosr = message.attachments.first().url
+            let osrdlfile = fs.createWriteStream('./files/replay.osr')
+            let requestw = https.get(`${attachosr}`, function (response) {
+                response.pipe(osrdlfile);
+
+                //console.log('success')
+            });
+            setTimeout(() => {
+                client.links.get('replayparse').execute(message, userdata, Discord, osuApiKey, osuClientID, osuClientSecret, config, currentDate, currentDateISO);
+            }, 1500)
+        }
+        if (messagenohttp.startsWith('osu.ppy.sh/scores/')) {
+            client.links.get('scoreparse').execute(message, userdata, Discord, osuApiKey, osuClientID, osuClientSecret, config, currentDate, currentDateISO);
         }
     })
 }
