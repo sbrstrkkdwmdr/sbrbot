e5c20817eb537082848b1e173d44ff84a3e08673
SaberStrike <sabersfear@gmail.com>
2022-06-21 20:05:53 +1000

    Update scoreparse.js

--- links/scoreparse.js
+++ links/scoreparse.js
@@ -10,11 +10,14 @@ module.exports = {
     execute(message, userdata, Discord, osuApiKey, osuClientID, osuClientSecret, config, currentDate, currentDateISO) {
         let messagenohttp = message.content.replace('https://', '').replace('http://', '').replace('www.', '')
 
-        //take info from the last '/' after the word "scores"
-        let scorelink = messagenohttp.split('/scores/')[1]
-        let scoremode = scorelink.split('/')[0]
-        let scoreid = scorelink.split('/')[1]
-        //console.log(scoremode, scoreid)
+        try {
+            let scorelink = messagenohttp.split('/scores/')[1]
+            let scoremode = scorelink.split('/')[0]
+            let scoreid = scorelink.split('/')[1]
+        } catch (error) {
+            return;
+        }
+
 
         let scoreurl = `https://osu.ppy.sh/api/v2/scores/${scoremode}/${scoreid}`
         fetch(scoreurl, {
@@ -24,118 +27,119 @@ module.exports = {
         }).then(res => res.json())
             .then(scoredata => {
                 fs.writeFileSync('debugosu/scoreparse.json', JSON.stringify(scoredata, null, 2));
+                fs.appendFileSync('link.log', `LINK DETECT EVENT - osu score link\n${currentDate} ${currentDateISO}\n${message.author.username}#${message.author.discriminator} (${message.author.id}) used osu!score link: ${message.content}\n`, 'utf-8')
 
-                (async () => {
+                    (async () => {
 
-                    let ranking = scoredata.rank
+                        let ranking = scoredata.rank
 
-                    switch (ranking) {
-                        case 'F':
-                            grade = 'F'
-                            break;
-                        case 'D':
-                            grade = '<:rankingD:927797179534438421>'
-                            break;
-                        case 'C':
-                            grade = '<:rankingC:927797179584757842>'
-                            break;
-                        case 'B':
-                            grade = '<:rankingB:927797179697991700>'
-                            break;
-                        case 'A':
-                            grade = '<:rankingA:927797179739930634>'
-                            break;
-                        case 'S':
-                            grade = '<:rankingS:927797179618295838>'
-                            break;
-                        case 'SH':
-                            grade = '<:rankingSH:927797179710570568>'
-                            break;
-                        case 'X':
-                            grade = '<:rankingX:927797179832229948>'
-                            break;
-                        case 'XH':
-                            grade = '<:rankingxh:927797179597357076>'
-                            break;
-                    };
-                    let gamehits = scoredata.statistics
+                        switch (ranking) {
+                            case 'F':
+                                grade = 'F'
+                                break;
+                            case 'D':
+                                grade = '<:rankingD:927797179534438421>'
+                                break;
+                            case 'C':
+                                grade = '<:rankingC:927797179584757842>'
+                                break;
+                            case 'B':
+                                grade = '<:rankingB:927797179697991700>'
+                                break;
+                            case 'A':
+                                grade = '<:rankingA:927797179739930634>'
+                                break;
+                            case 'S':
+                                grade = '<:rankingS:927797179618295838>'
+                                break;
+                            case 'SH':
+                                grade = '<:rankingSH:927797179710570568>'
+                                break;
+                            case 'X':
+                                grade = '<:rankingX:927797179832229948>'
+                                break;
+                            case 'XH':
+                                grade = '<:rankingxh:927797179597357076>'
+                                break;
+                        };
+                        let gamehits = scoredata.statistics
 
-                    let modint = 0
-                    if (scoredata.mods) {
-                        modint = osucalc.ModStringToInt(scoredata.mods.join(''))
-                    }
+                        let modint = 0
+                        if (scoredata.mods) {
+                            modint = osucalc.ModStringToInt(scoredata.mods.join(''))
+                        }
 
-                    let score = {
-                        beatmap_id: scoredata.beatmap.id,
-                        score: '6795149',
-                        maxcombo: '630',
-                        count50: gamehits.count_50,
-                        count100: gamehits.count_100,
-                        count300: gamehits.count_300,
-                        countmiss: '0',
-                        countkatu: gamehits.count_katu,
-                        countgeki: gamehits.count_geki,
-                        perfect: '1',
-                        enabled_mods: modint,
-                        user_id: scoredata.user_id,
-                        date: '2022-02-08 05:24:54',
-                        rank: 'S',
-                        score_id: '4057765057'
-                    }
+                        let score = {
+                            beatmap_id: scoredata.beatmap.id,
+                            score: '6795149',
+                            maxcombo: '630',
+                            count50: gamehits.count_50,
+                            count100: gamehits.count_100,
+                            count300: gamehits.count_300,
+                            countmiss: '0',
+                            countkatu: gamehits.count_katu,
+                            countgeki: gamehits.count_geki,
+                            perfect: '1',
+                            enabled_mods: modint,
+                            user_id: scoredata.user_id,
+                            date: '2022-02-08 05:24:54',
+                            rank: 'S',
+                            score_id: '4057765057'
+                        }
 
-                    let mode = scoredata.mode
+                        let mode = scoredata.mode
 
-                    if (mode == 'osu') {
-                        ppfc = new ppcalc.std_ppv2().setPerformance(score)
-                        hitlist = `${gamehits.count_300}/${gamehits.count_100}/${gamehits.count_50}/${gamehits.count_miss}`
-                        fcacc = osucalc.calcgrade(gamehits.count_300, gamehits.count_katu, gamehits.count_100, gamehits.count_50, gamehits.count_miss).accuracy
-                    }
-                    if (mode == 'taiko') {
-                        ppfc = new ppcalc.taiko_ppv2().setPerformance(score)
-                        hitlist = `${gamehits.count_300}/${gamehits.count_100}/${gamehits.count_miss}`
-                        fcacc = osucalc.calcgradeTaiko(gamehits.count_300, gamehits.count_100, gamehits.count_miss).accuracy
+                        if (mode == 'osu') {
+                            ppfc = new ppcalc.std_ppv2().setPerformance(score)
+                            hitlist = `${gamehits.count_300}/${gamehits.count_100}/${gamehits.count_50}/${gamehits.count_miss}`
+                            fcacc = osucalc.calcgrade(gamehits.count_300, gamehits.count_katu, gamehits.count_100, gamehits.count_50, gamehits.count_miss).accuracy
+                        }
+                        if (mode == 'taiko') {
+                            ppfc = new ppcalc.taiko_ppv2().setPerformance(score)
+                            hitlist = `${gamehits.count_300}/${gamehits.count_100}/${gamehits.count_miss}`
+                            fcacc = osucalc.calcgradeTaiko(gamehits.count_300, gamehits.count_100, gamehits.count_miss).accuracy
 
-                    }
-                    if (mode == 'fruits') {
-                        ppfc = new ppcalc.catch_ppv2().setPerformance(score)
-                        hitlist = `${gamehits.count_300}/${gamehits.count_100}/${gamehits.count_50}/${gamehits.count_miss}`
-                        fcacc = osucalc.calcgradeCatch(gamehits.count_300, gamehits.count_100, gamehits.count_50, gamehits.count_miss).accuracy
-                    }
-                    if (mode == 'mania') {
-                        ppfc = new ppcalc.mania_ppv2().setPerformance(score)
-                        hitlist = `${gamehits.count_geki}/${gamehits.count_300}/${gamehits.count_katu}/${gamehits.count_100}/${gamehits.count_50}/${gamehits.count_miss}`
-                        fcacc = osucalc.calcgradeMania(gamehits.count_geki, gamehits.count_300, gamehits.count_katu, gamehits.count_100, gamehits.count_50, gamehits.count_miss).accuracy
-                    }
+                        }
+                        if (mode == 'fruits') {
+                            ppfc = new ppcalc.catch_ppv2().setPerformance(score)
+                            hitlist = `${gamehits.count_300}/${gamehits.count_100}/${gamehits.count_50}/${gamehits.count_miss}`
+                            fcacc = osucalc.calcgradeCatch(gamehits.count_300, gamehits.count_100, gamehits.count_50, gamehits.count_miss).accuracy
+                        }
+                        if (mode == 'mania') {
+                            ppfc = new ppcalc.mania_ppv2().setPerformance(score)
+                            hitlist = `${gamehits.count_geki}/${gamehits.count_300}/${gamehits.count_katu}/${gamehits.count_100}/${gamehits.count_50}/${gamehits.count_miss}`
+                            fcacc = osucalc.calcgradeMania(gamehits.count_geki, gamehits.count_300, gamehits.count_katu, gamehits.count_100, gamehits.count_50, gamehits.count_miss).accuracy
+                        }
 
-                    let ppfc2 = await ppfc.compute()
+                        let ppfc2 = await ppfc.compute()
 
-                    let artist = scoredata.beatmapset.artist
-                    let artistuni = scoredata.beatmapset.artist_unicode
-                    let title = scoredata.beatmapset.title
-                    let titleuni = scoredata.beatmapset.title_unicode
+                        let artist = scoredata.beatmapset.artist
+                        let artistuni = scoredata.beatmapset.artist_unicode
+                        let title = scoredata.beatmapset.title
+                        let titleuni = scoredata.beatmapset.title_unicode
 
-                    if (artist != artistuni) {
-                        artist = `${artist} (${artistuni})`
-                    }
+                        if (artist != artistuni) {
+                            artist = `${artist} (${artistuni})`
+                        }
 
-                    if (title != titleuni) {
-                        title = `${title} (${titleuni})`
-                    }
+                        if (title != titleuni) {
+                            title = `${title} (${titleuni})`
+                        }
 
-                    let scoreembed = new Discord.MessageEmbed()
-                        .setColor('#0099ff')
-                        .setAuthor({ name: `${scoredata.user.username}`, iconURL: `https://a.ppy.sh/${scoredata.user.id}`, url: `https://osu.ppy.sh/users/${scoredata.user.id}` })
-                        .setTitle(`${artist} - ${title}`)
-                        .setURL(`https://osu.ppy.sh/b/${scoredata.beatmap.beatmap_id}`)
-                        .setThumbnail(`${scoredata.beatmapset.covers['list@2x']}`)
-                        .setDescription(`
+                        let scoreembed = new Discord.MessageEmbed()
+                            .setColor('#0099ff')
+                            .setAuthor({ name: `${scoredata.user.username}`, iconURL: `https://a.ppy.sh/${scoredata.user.id}`, url: `https://osu.ppy.sh/users/${scoredata.user.id}` })
+                            .setTitle(`${artist} - ${title}`)
+                            .setURL(`https://osu.ppy.sh/b/${scoredata.beatmap.beatmap_id}`)
+                            .setThumbnail(`${scoredata.beatmapset.covers['list@2x']}`)
+                            .setDescription(`
                         ${scoredata.accuracy.toFixed(2)}% | ${grade}
                         \`${hitlist}\`
                         ${scoredata.max_combo}x
                         ${scoredata.pp.toFixed(2)}pp | ${ppfc2.total.toFixed(2)}pp if ${fcacc}% FC
                         `)
-                    message.reply({ embeds: [scoreembed] })
-                })();
+                        message.reply({ embeds: [scoreembed] })
+                    })();
             })
     }
 }
