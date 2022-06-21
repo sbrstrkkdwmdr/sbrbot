e6a1b730f76c0cf500be52cb6d9b1629d2fad258
SaberStrike <sabersfear@gmail.com>
2022-06-21 17:50:05 +1000

    Update checkperms.js

--- commands-admin/checkperms.js
+++ commands-admin/checkperms.js
@@ -1,7 +1,11 @@
 const fs = require('fs')
 module.exports = {
     name: 'checkperms',
-    description: 'Checks the permissions of a user',
+    description: 'Checks the permissions of a user\n' + 
+    'Command: `sbr-checkperms <user>`\n' + 
+    'Slash Command: `/checkperms [user]`' + 
+    'Options: \n' +
+    '⠀⠀`user`: user/mention, optional. The user to check permissions of',
     execute(message, args, userdata, client, Discord, currentDate, currentDateISO, config, interaction) {
         if (message != null) {
             fs.appendFileSync('commands.log', `\nCOMMAND EVENT - checkperms (message)\n${currentDate} | ${currentDateISO}\n recieved checkperms command\nrequested by ${message.author.id} AKA ${message.author.tag}`, 'utf-8')
