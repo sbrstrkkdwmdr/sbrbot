673ff40afdfa53be53f86a550b12a7c7b879c766
SaberStrike <sabersfear@gmail.com>
2022-06-21 18:42:49 +1000

    Update setup.js

--- setup.js
+++ setup.js
@@ -2,12 +2,12 @@ const fs = require('fs')
 
 let config = 
 {
-    "token": "NzU1MjIwOTg5NDk0OTUxOTk3.X2AIWw.xEf7CivYo01i9M43L4sR49VlVuc",
+    "token": "null",
     "prefix": "sbr-",
-    "osuClientID": "11846",
-    "osuClientSecret": "upPsof8kuujr0rXDXs0eCtnrfl7iKF1j48ejCChc",
-    "osuApiKey": "d084ee2ffccb5c3317a686852442aec1cb04b657",
-    "testGuildID": "724514625005158400",
+    "osuClientID": "null",
+    "osuClientSecret": "null",
+    "osuApiKey": "null",
+    "testGuildID": "null",
     "ownerusers": []
 }
 fs.writeFileSync('./configs/config.json', JSON.stringify(config, null, 2), 'utf-8')
