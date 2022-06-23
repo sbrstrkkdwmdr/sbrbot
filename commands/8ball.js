4b456589582c6e08eca91f2267e57c8672d77cc3
SaberStrike <sabersfear@gmail.com>
2022-06-23 21:08:17 +1000

    fix logs + added some stuff

--- commands/8ball.js
+++ commands/8ball.js
@@ -1,3 +1,5 @@
+const fs = require('fs')
+
 module.exports = {
     name: '8ball',
     description: 'w',
@@ -5,17 +7,26 @@ module.exports = {
 
         let responses = [
             'yes', 'no', 'What? no', '知らない', 'nope', 'yeahhh', 'a strong maybe', 'definitely maybe not', 'nah', 'yeah of course', '多分', '絶対!!!',
-            'come again?', 'ehhhh', '⠀', '💀', '🥺', 'bruhhh', 'splish splash your question is trash'
+            'come again?', 'ehhhh', '⠀', '💀', '🥺', 'bruhhh', 'splish splash your question is trash', 3
         ]
 
         let q = responses[Math.floor(Math.random() * responses.length)]
+
+        if (q == 3) {
+            if (message != null) return message.channel.send('sbr-gif speech bubble')
+            if (interaction != null) {
+                interaction.channel.send('sbr-gif speech bubble')
+                interaction.reply({ content: ';)', ephemeral: true })
+                return;
+            }
+        }
 
         if (message != null) {
             fs.appendFileSync('commands.log', `\nCOMMAND EVENT - 8ball (message)\n${currentDate} | ${currentDateISO}\n recieved 8ball command\nrequested by ${message.author.id} AKA ${message.author.tag}`, 'utf-8')
             message.channel.send(q)
         }
         if (interaction != null) {
-            fs.appendFileSync('commands.log', `\nCOMMAND EVENT - 8ball (message)\n${currentDate} | ${currentDateISO}\n recieved 8ball command\nrequested by ${message.author.id} AKA ${message.author.tag}`, 'utf-8')
+            fs.appendFileSync('commands.log', `\nCOMMAND EVENT - 8ball (message)\n${currentDate} | ${currentDateISO}\n recieved 8ball command\nrequested by ${interaction.member.user.id} AKA ${interaction.member.user.tag}`, 'utf-8')
             interaction.reply(q)
         }
     }
--- commands/roll.js
+++ commands/roll.js
@@ -1,3 +1,5 @@
+const fs = require('fs')
+
 module.exports = {
     name: 'roll',
     description: 'w',
@@ -5,18 +7,18 @@ module.exports = {
         if (message != null) {
             fs.appendFileSync('commands.log', `\nCOMMAND EVENT - roll (message)\n${currentDate} | ${currentDateISO}\n recieved roll command\nrequested by ${message.author.id} AKA ${message.author.tag}`, 'utf-8')
             if (!args[0] || parseInt(args[0]) < 1) {
-                message.channels.send(Math.floor(Math.random() * 100 + 1))
+                message.channel.send(`${Math.floor(Math.random() * 100 + 1)}`)
             } else {
-                message.channels.send(Math.floor(Math.random() * args[0] + 1))
+                message.channel.send(`${Math.floor(Math.random() * args[0] + 1)}`)
             }
         }
         if (interaction != null) {
-            fs.appendFileSync('commands.log', `\nCOMMAND EVENT - roll (message)\n${currentDate} | ${currentDateISO}\n recieved roll command\nrequested by ${message.author.id} AKA ${message.author.tag}`, 'utf-8')
+            fs.appendFileSync('commands.log', `\nCOMMAND EVENT - roll (interaction)\n${currentDate} | ${currentDateISO}\n recieved roll command\nrequested by ${interaction.member.user.id} AKA ${interaction.member.user.tag}`, 'utf-8')
             let num = interaction.options.getNumber('number')
             if(!num){
-                interaction.reply(Math.floor(Math.random() * 100 + 1))
+                interaction.reply(`${Math.floor(Math.random() * 100 + 1)}`)
             } else {
-                interaction.reply(Math.floor(Math.random() * num + 1))
+                interaction.reply(`${Math.floor(Math.random() * num + 1)}`)
             }
         }
     }
