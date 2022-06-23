76b2e6339e02d9e0457dad51c1acc1e2c2919183
SaberStrike <sabersfear@gmail.com>
2022-06-23 10:48:57 +1000

    setup checks if config.json already exists

--- setup.js
+++ setup.js
@@ -8,7 +8,27 @@ let config =
     "osuClientSecret": "null",
     "osuApiKey": "null",
     "testGuildID": "null",
-    "ownerusers": []
+    "ownerusers": [],
+    "googlecx": "null", //https://cdn.discordapp.com/attachments/824898253005914112/892674375646584862/HowTo.mp4 to get it
+    "googlekey": "null" //https://cdn.discordapp.com/attachments/824898253005914112/892674375646584862/HowTo.mp4 to get it
+}
+if (fs.existsSync('./configs/config.json')){
+    try {
+        let configtest = JSON.parse(fs.readFileSync('./configs/config.json', 'utf-8'))
+        configtoken = configtest.token 
+        configprefix = configtest.prefix
+        configosuid = configtest.osuClientID
+        configosusecret = configtest.osuClientSecret
+        configosuapikey = configtest.osuApiKey
+        configtestguild = configtest.testGuildID
+        configowner = configtest.ownerusers
+        configgooglecx = configtest.googlecx
+        configgooglekey = configtest.googlekey
+        console.log('Config file already exists')
+        return;
+    } catch (error){
+
+    }
 }
 fs.writeFileSync('./configs/config.json', JSON.stringify(config, null, 2), 'utf-8')
 fs.writeFileSync('./configs/prevmap.json', JSON.stringify(({ id: 32345 }), null, 2), 'utf-8');
