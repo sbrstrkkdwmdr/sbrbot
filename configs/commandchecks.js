0cf5c1249c44984d3146fd968eb287d0b649ce54
SaberStrike <sabersfear@gmail.com>
2022-06-25 15:55:35 +1000

    add functions to check if a file is a video, image or audio

--- configs/commandchecks.js
+++ configs/commandchecks.js
@@ -1,24 +1,37 @@
 const fs = require('fs')
-//if user id is owner
-const owners = require('./config.json').ownerusers
+const config = require('./config.json')
 
 /**
  * 
  * @param {integer} userid 
- * @returns whether or not the user id is an owner
+ * @returns boolean
+ * @info checks whether or not the user is an owner
  */
 function isOwner(userid) {
-    for (let i = 0; i < owners.length; i++) {
-        if (owners[i] == userid) {
+    for (let i = 0; i < config.ownerusers.length; i++) {
+        if (config.ownerusers[i] == userid) {
             return true
         }
     }
     return false
 }
+
+/**
+ * 
+ * @param {string} cmd 
+ * @param {*} handler 
+ * @returns exec('console input')
+ * @info use it to put input into the console
+ */
 function exec(cmd, handler = function (error, stdout, stderr) { console.log(stdout); if (error !== null) { console.log(stderr) } }) {
     const childfork = require('child_process');
     return childfork.exec(cmd, handler);
 }
+/**
+ * 
+ * @param {string} txt 
+ * @returns shortens to under 65 characters
+ */
 function shorten(txt) {
     if (txt.length > 65) {
         newtxt = txt.substring(0, 64) + '...'
@@ -28,6 +41,11 @@ function shorten(txt) {
 
     return newtxt
 }
+/**
+ * 
+ * @param {string} txt 
+ * @returns shorten to under 150 characters
+ */
 function lengthshorten(txt) {
     if (txt.length > 150) {
         newtxt = txt.substring(0, 149) + '...'
@@ -37,6 +55,11 @@ function lengthshorten(txt) {
 
     return newtxt
 }
+/**
+ * 
+ * @param {string} txt 
+ * @returns shortened string to under 4000 characters to avoid discord message errors
+ */
 function discshort(txt) {
     if (txt.length > 4000) {
         newtxt = txt.substring(0, 3999)
@@ -46,5 +69,140 @@ function discshort(txt) {
 
     return newtxt
 }
+//file extensions for videos
+let vidfiletypes = [
+    "3g2",
+    "3gp",
+    "amv",
+    "asf",
+    "avi",
+    "drc",
+    "f4a",
+    "f4b",
+    "f4p",
+    "f4v",
+    "flv",
+    "gif",
+    "gifv",
+    "M2TS",
+    "m2v",
+    "m4p",
+    "m4v",
+    "mkv",
+    "mng",
+    "mov",
+    "mp2",
+    "mp4",
+    "mpe",
+    "mpeg",
+    "mpg",
+    "mpv",
+    "MTS",
+    "mxf",
+    "nsv",
+    "ogg",
+    "ogv",
+    "qv",
+    "rm",
+    "rmvb",
+    "roq",
+    "svi",
+    "TS",
+    "viv",
+    "vob",
+    "webm",
+    "wmv",
+    "yuv"
+]
 
-module.exports = { isOwner, exec, shorten, lengthshorten, discshort }
+//file types of images
+let imgfiletype = [
+    'apng',
+    'gif',
+    'jpeg',
+    'jpg',
+    'pdn',
+    'png'
+]
+
+let audiofiletype = [
+    'aac',
+    'flac',
+    'mp3',
+    'ogg',
+    'wav'
+]
+
+/**
+ * 
+ * @param {string} filename the name of the file (including extension)
+ * @returns whether or not a file is a video
+ */
+function checkisvideo(filename) {
+    for (let i = 0; i < vidfiletypes.length; i++) {
+        if (filename.url.indexOf(vidfiletypes[i], filename.url.length - vidfiletypes.length) !== -1) {
+            return true;
+        };
+
+    }
+
+    return false;
+}
+/**
+ * 
+ * @param {string} filename the name of the file (including extension)
+ * @returns whether or not a file is a video
+ */
+function checkisimage(filename) {
+    for (let i = 0; i < imgfiletype.length; i++) {
+        if (filename.url.indexOf(imgfiletype[i], filename.url.length - imgfiletype.length) !== -1) {
+            return true;
+        };
+
+    }
+
+    return false;
+}
+/**
+ * 
+ * @param {string} filename the name of the file (including extension)
+ * @returns whether or not a file is a video
+ */
+function checkisaudio(filename) {
+    for (let i = 0; i < audiofiletype.length; i++) {
+        if (filename.url.indexOf(audiofiletype[i], filename.url.length - audiofiletype.length) !== -1) {
+            return true;
+        };
+
+    }
+
+    return false;
+}
+/**
+ * 
+ * @param {int} userid 
+ * @returns check if user is banned from sending videos
+ */
+function checkisvidban(userid) {
+    for (let i = 0; i < config.blockvidusers.length; i++) {
+        if (config.blockvidusers[i] == userid) {
+            return true
+        }
+    }
+    return false
+}
+
+module.exports = {
+    audiofiletype,
+    checkisvidban,
+    checkisaudio,
+    checkisimage,
+    checkisvideo,
+    discshort,
+    exec,
+    imgfiletype,
+    isOwner,
+    lengthshorten,
+    shorten,
+    vidfiletypes,
+}
