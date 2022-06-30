ff09cd62308ef1cd194ce9028ce3402b187fcb38
SaberStrike <sabersfear@gmail.com>
2022-06-30 11:30:34 +1000

    music commands (WIP)

---
+++ commands-music/musicfunctions.js
@@ -0,0 +1,37 @@
+const discvoice = require('@discordjs/voice')
+const prism = require('prism-media')
+
+
+//discvoice.VoiceReceiver 
+function createListeningStream(receiver, userId, user) {
+	const opusStream = receiver.subscribe(userId, {
+		end: {
+			behavior: discvoice.EndBehaviorType.AfterSilence,
+			duration: 100,
+		},
+	});
+
+	const oggStream = new prism.opus.OggLogicalBitstream({
+		opusHead: new prism.opus.OpusHead({
+			channelCount: 2,
+			sampleRate: 48000,
+		}),
+		pageSizeControl: {
+			maxPackets: 10,
+		},
+	});
+
+	const filename = `./recordings/${Date.now()}-${getDisplayName(userId, user)}.ogg`;
+
+	const out = createWriteStream(filename);
+
+	console.log(`👂 Started recording ${filename}`);
+
+	pipeline(opusStream, oggStream, out, (err) => {
+		if (err) {
+			console.warn(`❌ Error recording file ${filename} - ${err.message}`);
+		} else {
+			console.log(`✅ Recorded ${filename}`);
+		}
+	});
+}
