5e50ee7c7566bf5cd90b2e6e01b79d2c3c909ad4
SaberStrike <sabersfear@gmail.com>
2022-07-02 21:27:20 +1000

    Update log.js

--- commands-admin/log.js
+++ commands-admin/log.js
@@ -4,7 +4,14 @@ module.exports = {
     description: 'returns the logs of the guild',
     execute(message, args, userdata, client, Discord, currentDate, currentDateISO, config, interaction) {
         if (message != null) {
+            fs.appendFileSync('commands.log', `\nCOMMAND EVENT - log (message)\n${currentDate} | ${currentDateISO}\n recieved get guild logs command\nrequested by ${message.author.id} AKA ${message.author.tag}\nMessage content: ${message.content}`, 'utf-8')
+
             message.reply({ files: [`./logs/${message.guild.id}.log`], allowedMentions: { repliedUser: false } });
+        }
+        //==============================================================================================================================================================================================
+        if(interaction != null){
+            fs.appendFileSync('commands.log', `\nCOMMAND EVENT - log (interaction)\n${currentDate} | ${currentDateISO}\n recieved get guild logs command\nrequested by ${interaction.member.user.id} AKA ${interaction.member.user.tag}`, 'utf-8')
+
         }
     }
 }
