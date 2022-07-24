ade485d6e8230b84a7f28ca957bbab5fe97a73f6
SaberStrike <71379681+sbrstrkkdwmdr@users.noreply.github.com>
2022-07-24 15:34:33 +1000

    Update ping.ts

--- commands/ping.ts
+++ commands/ping.ts
@@ -7,11 +7,11 @@ module.exports = {
     ,
     execute(message, userdata, client, Discord, currentDate, currentDateISO, config, interaction) {
         if (message != null) {
-            fs.appendFileSync('commands.log', `Event: ping (message command)\nTime: ${currentDate} | ${currentDateISO}`)
+            fs.appendFileSync('commands.log', `\nEvent: ping (message command)\nTime: ${currentDate} | ${currentDateISO}`)
             message.channel.send({ content: `Pong!\nLatency: ${Math.round(message.client.ws.ping)}ms`, allowedMentions: { repliedUser: false } });
         }
         if (interaction != null) {
-            fs.appendFileSync('commands.log', `Event: ping (interaction command)\nTime: ${currentDate} | ${currentDateISO}`)
+            fs.appendFileSync('commands.log', `\nEvent: ping (interaction command)\nTime: ${currentDate} | ${currentDateISO}`)
             interaction.reply({ content: `Pong!\nLatency: ${interaction.client.ws.ping}ms`, allowedMentions: { repliedUser: false } });
         }
 
