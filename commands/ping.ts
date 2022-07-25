8bbd3f4f6dc53b364c4f95b881d04d433d851b53
SaberStrike <71379681+sbrstrkkdwmdr@users.noreply.github.com>
2022-07-25 12:34:07 +1000

    Update ping.ts

--- commands/ping.ts
+++ commands/ping.ts
@@ -8,29 +8,28 @@ module.exports = {
         'Slash command: `/ping`'
     ,
     execute(message, userdata, client, Discord, currentDate, currentDateISO, config, interaction) {
-        let starttime = new Date ((fs.readFileSync('debug/starttime.txt')).toString())
-        let ifnew:any = ''
-        //if start time is under 30 minutes from the current time
-        if (starttime.getTime() + 1800000 > new Date().getTime()) {
-            ifnew = true
-        }
+        let starttime = new Date((fs.readFileSync('debug/starttime.txt')).toString())
 
         if (message != null) {
             fs.appendFileSync('commands.log', `\nEvent: ping (message command)\nTime: ${currentDate} | ${currentDateISO}\nrequested by ${message.author.id} AKA ${message.author.tag}\nMessage content: ${message.content}`)
-            message.channel.send({ content: `Pong!\nLatency: ${Math.round(message.client.ws.ping)}ms\n${ifnew}`, allowedMentions: { repliedUser: false } });
+            let trueping = message.createdAt.getTime() - new Date().getTime() + 'ms'
+
+            message.channel.send({ content: `Pong!\nClient latency: ${Math.round(client.ws.ping)}ms\nMessage Latency: ${trueping}`, allowedMentions: { repliedUser: false } });
         }
         if (interaction != null) {
             fs.appendFileSync('commands.log', `\nEvent: ping (interaction command)\nTime: ${currentDate} | ${currentDateISO}\nrequested by ${interaction.member.user.id} AKA ${interaction.member.user.tag}`)
 
-            let admininfo:any;
-            if(commandchecks.isOwner(interaction.member.user.id)) {
+            let admininfo: any = '';
+            let trueping = interaction.createdAt.getTime() - new Date().getTime() + 'ms'
+
+            if (commandchecks.isOwner(interaction.member.user.id)) {
                 let uptime = Math.round((new Date().getTime() - starttime.getTime()) / 1000);
                 let uptimehours = Math.floor(uptime / 3600) >= 10 ? Math.floor(uptime / 3600) : '0' + Math.floor(uptime / 3600);
                 let uptimeminutes = Math.floor((uptime % 3600) / 60) >= 10 ? Math.floor((uptime % 3600) / 60) : '0' + Math.floor((uptime % 3600) / 60);
                 let uptimeseconds = Math.floor(uptime % 60) >= 10 ? Math.floor(uptime % 60) : '0' + Math.floor(uptime % 60);
                 admininfo = `Uptime: ${uptimehours}:${uptimeminutes}:${uptimeseconds}\nTimezone: ${starttime.toString().split('(')[1].split(')')[0]}`
             }
-            interaction.reply({ content: `Pong!\nLatency: ${interaction.client.ws.ping}ms\n${admininfo}`, allowedMentions: { repliedUser: false } });
+            interaction.reply({ content: `Pong!\nClient latency: ${client.ws.ping}ms\nInteraction Latency:${trueping}\n${admininfo}`, allowedMentions: { repliedUser: false } });
         }
 
     },
