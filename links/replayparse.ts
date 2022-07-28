0acd10bb43ecae1bd1767719adc098bd4660a6d2
SaberStrike <71379681+sbrstrkkdwmdr@users.noreply.github.com>
2022-07-28 21:15:25 +1000

    Update replayparse.ts
    add hp graph
    stop bot from crashing if its set on a practice diff

--- links/replayparse.ts
+++ links/replayparse.ts
@@ -4,6 +4,8 @@ import replayparse = require('osureplayparser')
 import fetch from 'node-fetch';
 import { access_token } from '../configs/osuauth.json';
 import ppcalc = require('booba')
+import chartjsimg = require('chartjs-to-image');
+
 
 module.exports = {
     name: 'replayparse',
@@ -11,7 +13,7 @@ module.exports = {
     execute(message, userdata, Discord, osuApiKey, osuClientID, osuClientSecret, config, currentDate, currentDateISO) {
         //console.log('true')
         fs.appendFileSync('link.log', `LINK DETECT EVENT - replayparse\n${currentDate} ${currentDateISO}\n${message.author.username}#${message.author.discriminator} (${message.author.id}) used osu!score link: ${message.content}\n`, 'utf-8')
-        let replay:any;
+        let replay: any;
         try {
             replay = replayparse.parseReplay('./files/replay.osr')
         } catch (err) {
@@ -38,7 +40,7 @@ module.exports = {
             }).then(res => res.json() as any).then(userdata => {
 
                 fs.writeFileSync('debugosu/link-replayuser.json', JSON.stringify(userdata, null, 2))
-                let userid:any;
+                let userid: any;
                 try {
                     userid = userdata.id
                 } catch (err) {
@@ -46,29 +48,34 @@ module.exports = {
                     console.log(err)
                     return
                 }
-
-                let title = mapdata.beatmapset.title
-                let titleuni = mapdata.beatmapset.title_unicode
-                let artist = mapdata.beatmapset.artist
-                let artistuni = mapdata.beatmapset.artist_unicode
-
-                if (title != titleuni) {
-                    title = `${title} (${titleuni})`
-                }
-                if (artist != artistuni) {
-                    artist = `${artist} (${artistuni})`
+                let title:any;
+                let artist:any;
+                let version:any;
+                let mapbg:any;
+                let mapcombo:string|number;
+                try {
+                    title = mapdata.beatmapset.title != mapdata.beatmapset.title_unicode ? `${mapdata.beatmapset.title} (${mapdata.beatmapset.title_unicode})` : mapdata.beatmapset.title
+                    artist = mapdata.beatmapset.artist != mapdata.beatmapset.artist_unicode ? `${mapdata.beatmapset.artist} (${mapdata.beatmapset.artist_unicode})` : mapdata.beatmapset.artist
+                    version = mapdata.version
+                    mapbg = mapdata.beatmapset.covers['list@2x']
+                    mapcombo = mapdata.max_combo ? mapdata.max_combo.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") : NaN
+                } catch (error){
+                    title = 'Unknown title'
+                    artist = 'Unknown artist'
+                    version = 'Unknown version'
+                    mapbg = 'https://osu.ppy.sh/images/layout/avatar-guest@2x.png';
                 }
 
                 let mods = replay.mods
-                let ifmods:any;
+                let ifmods: any;
                 if (mods != 0) {
                     ifmods = `+${osucalc.ModIntToString(mods)}`
                 } else {
                     ifmods = ''
                 }
                 let gameMode = replay.gameMode
-                let hitlist:any;
-                let accuracy:any;
+                let hitlist: any;
+                let accuracy: any;
                 switch (gameMode) {
                     case 0:
                         hitlist = `${replay.number_300s}/${replay.number_100s}/${replay.number_50s}/${replay.misses}`
@@ -88,21 +95,53 @@ module.exports = {
                         break;
 
                 }
+                let lifebar = replay.life_bar.split('|')
+                let lifebarF: any[] = []
+                for (let i = 0; i < lifebar.length; i++) {
+                    lifebarF.push(lifebar[i].split(',')[0])
+                }
+                lifebarF.shift()
 
-                let Embed = new Discord.EmbedBuilder()
-                    .setColor('#0099ff')
-                    .setAuthor({ name: `${replay.playerName}'s replay`, iconURL: `https://a.ppy.sh/${userid}`, url: `https://osu.ppy.sh/users/${userid}` })
-                    .setTitle(`Replay`)
-                    .setThumbnail(mapdata.beatmapset.covers['list@2x'])
-                    .setDescription(
-                        `[${artist} - ${title} [${mapdata.version}]](https://osu.ppy.sh/b/${mapdata.id}) ${ifmods}
-                    ${replay.score.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")} | ${replay.max_combo.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}x/**${mapdata.max_combo.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}x** | ${accuracy.toFixed(2)}
+                let dataLabel = ['Start']
+
+                for (let i = 0; i < lifebarF.length; i++) {
+                    dataLabel.push('')
+
+                }
+
+                dataLabel.push('end')
+
+                const chart = new chartjsimg()
+                    .setConfig({
+                        type: 'line',
+                        data: {
+                            labels: dataLabel,
+                            datasets: [{
+                                label: 'Health',
+                                data: lifebarF,
+                                fill: false,
+                                borderColor: 'rgb(75, 192, 192)',
+                                borderWidth: 1,
+                                pointRadius: 0
+                            }]
+                        }
+                    })
+                chart.setBackgroundColor('color: rgb(0,0,0)')
+                chart.toFile('./debugosu/replaygraph.jpg').then(() => {
+                    let Embed = new Discord.EmbedBuilder()
+                        .setColor('#0099ff')
+                        .setAuthor({ name: `${replay.playerName}'s replay`, iconURL: `https://a.ppy.sh/${userid}`, url: `https://osu.ppy.sh/users/${userid}` })
+                        .setTitle(`Replay`)
+                        .setThumbnail(mapbg)
+                        .setDescription(
+                            `[${artist} - ${title} [${version}]](https://osu.ppy.sh/b/${mapdata.id}) ${ifmods}
+                    ${replay.score.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")} | ${replay.max_combo.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}x/**${mapcombo}x** | ${accuracy.toFixed(2)}
                     \`${hitlist}\`
                     `
-                    )
+                        )
 
-                message.reply({ embeds: [Embed], allowedMentions: { repliedUser: false } })
-
+                    message.reply({ embeds: [Embed], files: ['./debugosu/replaygraph.jpg'], allowedMentions: { repliedUser: false } })
+                })
             })
 
         })
