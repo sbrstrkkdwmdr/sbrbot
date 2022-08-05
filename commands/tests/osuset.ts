6e1ada9dc6a69d7a42a45811ba0ed17998d1db9a
SaberStrike <71379681+sbrstrkkdwmdr@users.noreply.github.com>
2022-08-05 17:01:43 +1000

    wip

---
+++ commands/tests/osuset.ts
@@ -0,0 +1,169 @@
+import fs = require('fs')
+module.exports = {
+    name: 'osuset',
+    description: 'Sets the osu! username of the user\n' +
+        'Command: `sbr-osuset <username>`\n' +
+        'Slash Command: `/osuset [username] [mode]`\n' +
+        'Options:\n' +
+        '⠀⠀`username`: string, required. The osu! username of the user\n' +
+        '⠀⠀`mode`: string, optional. The mode of the user. If omitted, the default mode \'osu\' will be used.\n',
+    async execute(message, args, userdata, client, Discord, currentDate, currentDateISO, config, interaction, absoluteID, button, obj) {
+
+        if (message != null) {
+            let username = args.join(' ')
+            if (!args[0]) return message.reply({ content: 'Please enter a username or ID', allowedMentions: { repliedUser: false }, failIfNotExists: true })
+            //message.reply({ content: 'please use the slash command instead', allowedMentions: { repliedUser: false }, failIfNotExists: true })
+            try {
+                await userdata.create({
+                    userid: message.author.id,
+                    osuname: username,
+                    mode: 'osu',
+                })
+                message.reply({ content: 'added to the database', allowedMentions: { repliedUser: false } })
+                fs.appendFileSync(`logs/cmd/commands${obj.guildId}.log`, `\nsuccess\n\n`, 'utf-8')
+                fs.appendFileSync(`logs/cmd/commands${obj.guildId}.log`, `\nCommand Information\nusername: ${username}\n`)
+                let endofcommand = new Date().getTime();
+                let timeelapsed = endofcommand - currentDate.getTime();
+                fs.appendFileSync(`logs/cmd/commands${obj.guildId}.log`, `\nCommand Latency - ${timeelapsed}ms\n`)
+
+            } catch (error) {
+                let affectedRows = await userdata.update({
+                    osuname: username,
+                    mode: 'osu',
+                }, { where: { userid: message.author.id } })
+                if (affectedRows > 0) {
+                    fs.appendFileSync(`logs/cmd/commands${obj.guildId}.log`, `\nsuccess\n\n`, 'utf-8')
+                    fs.appendFileSync(`logs/cmd/commands${obj.guildId}.log`, `\nCommand Information\nusername: ${username}`)
+                    let endofcommand = new Date().getTime();
+                    let timeelapsed = endofcommand - currentDate.getTime();
+                    fs.appendFileSync(`logs/cmd/commands${obj.guildId}.log`, `\nCommand Latency - ${timeelapsed}ms\n`)
+                    return message.reply({ content: 'updated the database', allowedMentions: { repliedUser: false } })
+                }
+                let endofcommand = new Date().getTime();
+                let timeelapsed = endofcommand - currentDate.getTime();
+                fs.appendFileSync(`logs/cmd/commands${obj.guildId}.log`, `\nCommand Latency - ${timeelapsed}ms\n`)
+                return message.reply({ content: 'failed to update the database', allowedMentions: { repliedUser: false } })
+
+
+            }
+        }
+
+        if (interaction != null) {
+            fs.appendFileSync(`logs/cmd/commands${obj.guildId}.log`, `\nCOMMAND EVENT - osuset (interaction)\n${currentDate} | ${currentDateISO}\n recieved set osu! username command\nrequested by ${interaction.member.user.id} AKA ${interaction.member.user.tag}`, 'utf-8')
+            fs.appendFileSync(`logs/cmd/commands${obj.guildId}.log`, `\nInteraction ID: ${interaction.id}`)
+            fs.appendFileSync(`logs/cmd/commands${obj.guildId}.log`,
+                `\noptions:
+            user: ${interaction.options.getString('user')}
+            mode: ${interaction.options.getString('mode')}
+            `)
+            let username = interaction.options.getString('user');
+            let mode = interaction.options.getString('mode') + '';
+            let skin = interaction.options.getString('skin');
+            if (mode.length < 1 && !skin) {
+                try {
+                    await userdata.create({
+                        userid: interaction.member.user.id,
+                        osuname: username,
+                        mode: 'osu',
+                    })
+                    interaction.reply({ content: 'added to the database', allowedMentions: { repliedUser: false } })
+                    fs.appendFileSync(`logs/cmd/commands${obj.guildId}.log`, `\nsuccess - Interaction ID: ${interaction.id}\n\n`, 'utf-8')
+                    fs.appendFileSync(`logs/cmd/commands${obj.guildId}.log`, `\nCommand Information\nusername: ${username}\nmode: ${mode}`)
+
+                    let endofcommand = new Date().getTime();
+                    let timeelapsed = endofcommand - currentDate.getTime();
+                    fs.appendFileSync(`logs/cmd/commands${obj.guildId}.log`, `\nCommand Latency - ${timeelapsed}ms\n`)
+                } catch (error) {
+                    let affectedRows = await userdata.update({
+                        osuname: username,
+                        mode: 'osu',
+                    }, { where: { userid: interaction.member.user.id } })
+                    if (affectedRows > 0) {
+                        fs.appendFileSync(`logs/cmd/commands${obj.guildId}.log`, `\nsuccess - ${interaction.id}\n\n`, 'utf-8')
+                        fs.appendFileSync(`logs/cmd/commands${obj.guildId}.log`, `\nCommand Information\nusername: ${username}\nmode: ${mode}`)
+                        let endofcommand = new Date().getTime();
+                        let timeelapsed = endofcommand - currentDate.getTime();
+                        fs.appendFileSync(`logs/cmd/commands${obj.guildId}.log`, `\nCommand Latency - ${timeelapsed}ms\n`)
+                        return interaction.reply({ content: 'updated the database', allowedMentions: { repliedUser: false } })
+                    }
+                    let endofcommand = new Date().getTime();
+                    let timeelapsed = endofcommand - currentDate.getTime();
+                    fs.appendFileSync(`logs/cmd/commands${obj.guildId}.log`, `\nCommand Latency - ${timeelapsed}ms\n`)
+                    return interaction.reply({ content: 'failed to update the database', allowedMentions: { repliedUser: false } })
+                }
+            }
+            else if (!skin) {
+                try {
+                    await userdata.create({
+                        userid: interaction.member.user.id,
+                        osuname: username,
+                        mode: mode,
+                    })
+                    interaction.reply({ content: 'added to the database', allowedMentions: { repliedUser: false } })
+                    fs.appendFileSync(`logs/cmd/commands${obj.guildId}.log`, `\nsuccess - ${interaction.id}\n\n`, 'utf-8')
+                    fs.appendFileSync(`logs/cmd/commands${obj.guildId}.log`, `\nCommand Information\nusername: ${username}\nmode: ${mode}`)
+                    let endofcommand = new Date().getTime();
+                    let timeelapsed = endofcommand - currentDate.getTime();
+                    fs.appendFileSync(`logs/cmd/commands${obj.guildId}.log`, `\nCommand Latency - ${timeelapsed}ms\n`)
+
+                } catch (error) {
+                    let affectedRows = await userdata.update({
+                        osuname: username,
+                        mode: mode,
+                    }, { where: { userid: interaction.member.user.id } })
+                    if (affectedRows > 0) {
+                        fs.appendFileSync(`logs/cmd/commands${obj.guildId}.log`, `\nsuccess - ${interaction.id}\n\n`, 'utf-8')
+                        fs.appendFileSync(`logs/cmd/commands${obj.guildId}.log`, `\nCommand Information\nusername: ${username}\nmode: ${mode}`)
+                        let endofcommand = new Date().getTime();
+                        let timeelapsed = endofcommand - currentDate.getTime();
+                        fs.appendFileSync(`logs/cmd/commands${obj.guildId}.log`, `\nCommand Latency - ${timeelapsed}ms\n`)
+                        return interaction.reply({ content: 'updated the database', allowedMentions: { repliedUser: false } })
+                    }
+                    let endofcommand = new Date().getTime();
+                    let timeelapsed = endofcommand - currentDate.getTime();
+                    fs.appendFileSync(`logs/cmd/commands${obj.guildId}.log`, `\nCommand Latency - ${timeelapsed}ms\n`)
+                    return interaction.reply({ content: 'failed to update the database', allowedMentions: { repliedUser: false } })
+
+
+                }
+            }
+            else if (skin) {
+                try {
+                    await userdata.create({
+                        userid: interaction.member.user.id,
+                        osuname: username,
+                        mode: mode,
+                        skin: skin
+                    })
+                    interaction.reply({ content: 'added skin to the database', allowedMentions: { repliedUser: false } })
+                    fs.appendFileSync(`logs/cmd/commands${obj.guildId}.log`, `\nsuccess - ${interaction.id}\n\n`, 'utf-8')
+                    fs.appendFileSync(`logs/cmd/commands${obj.guildId}.log`, `\nCommand Information\nusername: ${username}\nmode: ${mode}`)
+                    let endofcommand = new Date().getTime();
+                    let timeelapsed = endofcommand - currentDate.getTime();
+                    fs.appendFileSync(`logs/cmd/commands${obj.guildId}.log`, `\nCommand Latency - ${timeelapsed}ms\n`)
+
+                } catch (error) {
+                    let affectedRows = await userdata.update({
+                        osuname: username,
+                        mode: mode,
+                        skin: skin
+                    }, { where: { userid: interaction.member.user.id } })
+                    if (affectedRows > 0) {
+                        fs.appendFileSync(`logs/cmd/commands${obj.guildId}.log`, `\nsuccess - ${interaction.id}\n\n`, 'utf-8')
+                        fs.appendFileSync(`logs/cmd/commands${obj.guildId}.log`, `\nCommand Information\nusername: ${username}\nmode: ${mode}`)
+                        let endofcommand = new Date().getTime();
+                        let timeelapsed = endofcommand - currentDate.getTime();
+                        fs.appendFileSync(`logs/cmd/commands${obj.guildId}.log`, `\nCommand Latency - ${timeelapsed}ms\n`)
+                        return interaction.reply({ content: 'updated the database', allowedMentions: { repliedUser: false } })
+                    }
+                    let endofcommand = new Date().getTime();
+                    let timeelapsed = endofcommand - currentDate.getTime();
+                    fs.appendFileSync(`logs/cmd/commands${obj.guildId}.log`, `\nCommand Latency - ${timeelapsed}ms\n`)
+                    return interaction.reply({ content: 'failed to update the database', allowedMentions: { repliedUser: false } })
+
+
+                }
+            }
+        }
+    }
+}
---
+++ commands/tests/osutop.ts
@@ -0,0 +1,92 @@
+import fs = require('fs');
+import osumodcalc = require('osumodcalculator');
+import osuapiext = require('osu-api-extended');
+import { access_token } from '../../configs/osuauth.json';
+import fetch from 'node-fetch';
+module.exports = {
+    name: 'template',
+    description: 'template text\n' +
+        'Command: `sbr-command-name`\n' +
+        'Options: \n' +
+        '    `--option-name`: `option-description`\n',
+    execute(message, args, userdata, client, Discord, currentDate, currentDateISO, config, interaction, absoluteID, button) {
+        //let absoluteID = new Date().getTime()
+        let accessN = fs.readFileSync('configs/osuauth.json', 'utf-8');
+        let access_token = JSON.parse(accessN).access_token;
+        
+        if (message != null) {
+            fs.appendFileSync(`logs/cmd/commands${message.guildId}.log`, `\nCOMMAND EVENT - COMMANDNAME (message)\n${currentDate} | ${currentDateISO}\n recieved COMMANDNAME command\nrequested by ${message.author.id} AKA ${message.author.tag}\nMessage content: ${message.content}`, 'utf-8')
+            let buttons = new Discord.ActionRowBuilder()
+                .addComponents(
+                    new Discord.ButtonBuilder()
+                        .setCustomId(`BigLeftArrow-cmd-${message.author.id}`)
+                        .setStyle('Primary')
+                        .setEmoji('⬅')
+                    /* .setLabel('Start') */,
+                    new Discord.ButtonBuilder()
+                        .setCustomId(`LeftArrow-cmd-${message.author.id}`)
+                        .setStyle('Primary')
+                        .setEmoji('◀')
+                    /* .setLabel('Previous') */,
+                    /*                 new Discord.ButtonBuilder()
+                                        .setCustomId('Middle-cmd')
+                                        .setStyle('Primary')
+                                        .setLabel('🔍')
+                                    , */
+                    new Discord.ButtonBuilder()
+                        .setCustomId(`RightArrow-cmd-${message.author.id}`)
+                        .setStyle('Primary')
+                        .setEmoji('▶')
+                    /* .setLabel('Next') */,
+                    new Discord.ButtonBuilder()
+                        .setCustomId(`BigRightArrow-cmd-${message.author.id}`)
+                        .setStyle('Primary')
+                        .setEmoji('➡')
+                    /* .setLabel('End') */,
+                );
+            let endofcommand = new Date().getTime();
+            let timeelapsed = endofcommand - currentDate.getTime();
+            fs.appendFileSync(`logs/cmd/commands${message.guildId}.log`, `\nCommand Latency - ${timeelapsed}ms\n`)
+        }
+
+        //==============================================================================================================================================================================================
+
+        if (interaction != null) {
+            fs.appendFileSync(`logs/cmd/commands${interaction.guildId}.log`, `\nCOMMAND EVENT - COMMANDNAME (interaction)\n${currentDate} | ${currentDateISO}\n recieved COMMANDNAME command\nrequested by ${interaction.member.user.id} AKA ${interaction.member.user.tag}`, 'utf-8')
+
+            let buttons = new Discord.ActionRowBuilder()
+                .addComponents(
+                    new Discord.ButtonBuilder()
+                        .setCustomId(`BigLeftArrow-cmd-${interaction.user.id}`)
+                        .setStyle('Primary')
+                        .setEmoji('⬅')
+                    /* .setLabel('Start') */,
+                    new Discord.ButtonBuilder()
+                        .setCustomId(`LeftArrow-cmd-${interaction.user.id}`)
+                        .setStyle('Primary')
+                        .setEmoji('◀')
+                    /* .setLabel('Previous') */,
+                    /*                 new Discord.ButtonBuilder()
+                                        .setCustomId('Middle-cmd')
+                                        .setStyle('Primary')
+                                        .setLabel('🔍')
+                                    , */
+                    new Discord.ButtonBuilder()
+                        .setCustomId(`RightArrow-cmd-${interaction.user.id}`)
+                        .setStyle('Primary')
+                        .setEmoji('▶')
+                    /* .setLabel('Next') */,
+                    new Discord.ButtonBuilder()
+                        .setCustomId(`BigRightArrow-cmd-${interaction.user.id}`)
+                        .setStyle('Primary')
+                        .setEmoji('➡')
+                    /* .setLabel('End') */,
+                );
+            let endofcommand = new Date().getTime();
+            let timeelapsed = endofcommand - currentDate.getTime();
+            fs.appendFileSync(`logs/cmd/commands${interaction.guildId}.log`, `\nCommand Latency - ${timeelapsed}ms\n`)
+        }
+
+        fs.appendFileSync(`logs/cmd/commands${interaction.guildId}.log`, '\nsuccess\n\n', 'utf-8')
+    }
+}
