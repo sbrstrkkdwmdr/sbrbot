ddb444a3ad0f7d91a99809711c409ae6c6b5591a
SaberStrike <71379681+sbrstrkkdwmdr@users.noreply.github.com>
2022-08-06 18:16:51 +1000

    w

--- commands/tests/rs.ts
+++ commands/tests/rs.ts
@@ -1,22 +1,25 @@
 import fs = require('fs');
 import osumodcalc = require('osumodcalculator');
-import osuapiext = require('osu-api-extended');
-import { access_token } from '../../configs/osuauth.json';
+import ppcalc = require('booba');
 import fetch from 'node-fetch';
+import emojis = require('../../configs/emojis');
+import osufunc = require('../../configs/osufunc');
+import cmdchecks = require('../../configs/commandchecks');
 module.exports = {
     name: 'template',
     description: 'template text\n' +
         'Command: `sbr-command-name`\n' +
         'Options: \n' +
         '    `--option-name`: `option-description`\n',
-    execute(message, args, userdata, client, Discord, currentDate, currentDateISO, config, interaction, absoluteID, button) {
+    execute(message, args, userdata, client, Discord, currentDate, currentDateISO, config, interaction, absoluteID, button, obj) {
         //let absoluteID = new Date().getTime()
         let accessN = fs.readFileSync('configs/osuauth.json', 'utf-8');
         let access_token = JSON.parse(accessN).access_token;
-        
-        if (message != null) {
+        let buttons;
+
+        if (message != null && button == null) {
             fs.appendFileSync(`logs/cmd/commands${message.guildId}.log`, `\nCOMMAND EVENT - COMMANDNAME (message)\n${currentDate} | ${currentDateISO}\n recieved COMMANDNAME command\nrequested by ${message.author.id} AKA ${message.author.tag}\nMessage content: ${message.content}`, 'utf-8')
-            let buttons = new Discord.ActionRowBuilder()
+            buttons = new Discord.ActionRowBuilder()
                 .addComponents(
                     new Discord.ButtonBuilder()
                         .setCustomId(`BigLeftArrow-cmd-${message.author.id}`)
@@ -44,17 +47,14 @@ module.exports = {
                         .setEmoji('➡')
                     /* .setLabel('End') */,
                 );
-            let endofcommand = new Date().getTime();
-            let timeelapsed = endofcommand - currentDate.getTime();
-            fs.appendFileSync(`logs/cmd/commands${message.guildId}.log`, `\nCommand Latency - ${timeelapsed}ms\n`)
         }
 
         //==============================================================================================================================================================================================
 
-        if (interaction != null) {
+        if (interaction != null && button == null) {
             fs.appendFileSync(`logs/cmd/commands${interaction.guildId}.log`, `\nCOMMAND EVENT - COMMANDNAME (interaction)\n${currentDate} | ${currentDateISO}\n recieved COMMANDNAME command\nrequested by ${interaction.member.user.id} AKA ${interaction.member.user.tag}`, 'utf-8')
 
-            let buttons = new Discord.ActionRowBuilder()
+            buttons = new Discord.ActionRowBuilder()
                 .addComponents(
                     new Discord.ButtonBuilder()
                         .setCustomId(`BigLeftArrow-cmd-${interaction.user.id}`)
@@ -82,11 +82,46 @@ module.exports = {
                         .setEmoji('➡')
                     /* .setLabel('End') */,
                 );
-            let endofcommand = new Date().getTime();
-            let timeelapsed = endofcommand - currentDate.getTime();
-            fs.appendFileSync(`logs/cmd/commands${interaction.guildId}.log`, `\nCommand Latency - ${timeelapsed}ms\n`)
         }
 
-        fs.appendFileSync(`logs/cmd/commands${interaction.guildId}.log`, '\nsuccess\n\n', 'utf-8')
+        //==============================================================================================================================================================================================
+
+        if (button != null){
+            fs.appendFileSync(`logs/cmd/commands${interaction.guildId}.log`, `\nCOMMAND EVENT - COMMANDNAME (button)\n${currentDate} | ${currentDateISO}\n recieved COMMANDNAME command\nrequested by ${interaction.member.user.id} AKA ${interaction.member.user.tag}`, 'utf-8')
+            buttons = new Discord.ActionRowBuilder()
+            .addComponents(
+                new Discord.ButtonBuilder()
+                    .setCustomId(`BigLeftArrow-cmd-${interaction.user.id}`)
+                    .setStyle('Primary')
+                    .setEmoji('⬅')
+                /* .setLabel('Start') */,
+                new Discord.ButtonBuilder()
+                    .setCustomId(`LeftArrow-cmd-${interaction.user.id}`)
+                    .setStyle('Primary')
+                    .setEmoji('◀')
+                /* .setLabel('Previous') */,
+                /*                 new Discord.ButtonBuilder()
+                                    .setCustomId('Middle-cmd')
+                                    .setStyle('Primary')
+                                    .setLabel('🔍')
+                                , */
+                new Discord.ButtonBuilder()
+                    .setCustomId(`RightArrow-cmd-${interaction.user.id}`)
+                    .setStyle('Primary')
+                    .setEmoji('▶')
+                /* .setLabel('Next') */,
+                new Discord.ButtonBuilder()
+                    .setCustomId(`BigRightArrow-cmd-${interaction.user.id}`)
+                    .setStyle('Primary')
+                    .setEmoji('➡')
+                /* .setLabel('End') */,
+            );
+        }
+
+
+        let endofcommand = new Date().getTime();
+        let timeelapsed = endofcommand - currentDate.getTime();
+        fs.appendFileSync(`logs/cmd/commands${obj.guildId}.log`, `\nCommand Latency - ${timeelapsed}ms\n`)
+        fs.appendFileSync(`logs/cmd/commands${obj.guildId}.log`, '\nsuccess\n\n', 'utf-8')
     }
 }
