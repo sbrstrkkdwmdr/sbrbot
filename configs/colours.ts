8114b6857171e9a29c252f71b8121ac3a71520bc
SaberStrike <71379681+sbrstrkkdwmdr@users.noreply.github.com>
2022-08-12 16:44:37 +1000

    w

---
+++ configs/colours.ts
@@ -0,0 +1,97 @@
+let diffcolour: any[] = [
+    {
+        decimal: '5554943',
+        hex: '54C2FF',
+        rgb: '61,186,255', //0-255,0-255,0-255
+        hsv: '201,76,100', //0-360, 0-100, 0-100
+        cym: ''
+    }, //s>1.5>s
+    {
+        decimal: '5373909',
+        hex: '51FFD5',
+        rgb: '81,255,213',
+        hsv: '165,68,100',
+        cym: ''
+    }, //s>2.0
+    {
+        decimal: '7601987',
+        hex: '73FF43',
+        rgb: '115,255,67',
+        hsv: '104,73,100',
+        cym: ''
+    }, //s>2.5
+    {
+        decimal: '16183392',
+        hex: 'F6F060',
+        rgb: '246,240,96',
+        hsv: '57,60,96',
+        cym: ''
+    }, //s>3.25
+    {
+        decimal: '16744809',
+        hex: 'FF8169',
+        rgb: '255,129,105',
+        hsv: '9,58,100',
+        cym: ''
+    }, //s>4.5
+    {
+        decimal: '16726640',
+        hex: 'FF3A70',
+        rgb: '255,58,112',
+        hsv: '343,77,100',
+        cym: ''
+    }, //s>6.0
+    {
+        decimal: '6644702',
+        hex: '6563DE',
+        rgb: '101,99,222',
+        hsv: '240,55,87',
+        cym: ''
+    }, //s>7.0
+    {
+        decimal: '0',
+        hex: '000000',
+        rgb: '0,0,0',
+        hsv: '0,0,0',
+        cym: ''
+    }, //s>8.0
+
+]
+
+let embedColour: any[] = [
+    {
+        name: 'score',
+        hex: 'DF7FFF',
+        dec: '',
+    },
+    {
+        name: 'scorelist',
+        hex: '8A7FFF',
+        dec: '',
+    },
+    {
+        name: 'user',
+        hex: '16D8FF',
+        dec: '',
+    },
+    {
+        name: 'playerlist',
+        hex: '0099FF',
+        dec: '',
+    },
+    {
+        name: 'query',
+        hex: '000099',
+        dec: '',
+    },
+    {
+        name: 'admin',
+        hex: '',
+        dec: ''
+    },
+    {
+        name: 'misc',
+        hex: '',
+        dec: '',
+    },
+]
