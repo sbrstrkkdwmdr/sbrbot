325c0b0eeb2e0343405e36b6b88069ef731db001
SaberStrike <71379681+sbrstrkkdwmdr@users.noreply.github.com>
2022-08-16 17:54:59 +1000

    fix error handling

--- commands/links/localmapparse.ts
+++ commands/links/localmapparse.ts
@@ -28,6 +28,7 @@ cmd ID: ${absoluteID}
             return;
         }
         let mods = 'NM'
+
         if (message.content.includes('+')) {
             mods = message.content.split('+')[1].split(' ')[0]
         }
@@ -41,10 +42,27 @@ cmd ID: ${absoluteID}
                 failIfNotExists: true
             })
         }
-        let metadata = map.split('[Metadata]')[1].split('[')[0]
-
+        let metadata;
+        try {
+            metadata = map.split('[Metadata]')[1].split('[')[0]
+        } catch (error) {
+            await message.reply({
+                content: `Error - invalid .osu file`,
+                allowedMentions: { repliedUser: false },
+            })
+            return;
+        }
         if (metadata.includes('BeatmapID')) {
-            let bid = metadata.split('BeatmapID')[1].split('\n')[0]
+            let bid;
+            try {
+                bid = metadata.split('BeatmapID')[1].split('\n')[0]
+            } catch (error) {
+                await message.reply({
+                    content: `Error - invalid .osu file`,
+                    allowedMentions: { repliedUser: false },
+                })
+                return;
+            }
             if (parseInt(bid) != NaN && parseInt(bid) > 0) {
                 let parse = null;
                 let overrideID = cmdchecks.toAlphaNum(bid)
@@ -61,50 +79,103 @@ overrideID: ${overrideID}
                 return;
             }
         }
-        let ppcalcing = await osufunc.mapcalclocal(mods, 'osu', null, 0)
+        let ppcalcing;
+        try {
+            ppcalcing = await osufunc.mapcalclocal(mods, 'osu', null, 0)
+        } catch (error) {
+            await message.reply({
+                content: `Error - invalid .osu file`,
+                allowedMentions: { repliedUser: false },
+            })
+            return;
+        }
 
         let ftstr: string;
+        let general;
+        let diff;
+        try {
+            general = map.split('[General]')[1].split('[')[0]
+            diff = map.split('[Difficulty]')[1].split('[')[0]
+        } catch (error) {
+            await message.reply({
+                content: `Error - invalid .osu file`,
+                allowedMentions: { repliedUser: false },
+            })
+            return;
+        }
+        let title;
+        let artist;
+        let creator;
+        let version;
+        try {
+            title = metadata.split('Title:')[1].split('\n')[0]
+                ==
+                metadata.split('Title:')[1].split('\n')[0]
+                ?
+                metadata.split('TitleUnicode:')[1].split('\n')[0] :
+                `${metadata.split('Title:')[1].split('\n')[0]} (${metadata.split('TitleUnicode:')[1].split('\n')[0]})`
+            artist = metadata.split('Artist:')[1].split('\n')[0]
+                ==
+                metadata.split('ArtistUnicode:')[1].split('\n')[0]
+                ?
+                metadata.split('ArtistUnicode:')[1].split('\n')[0] :
+                `${metadata.split('Artist:')[1].split('\n')[0]} (${metadata.split('ArtistUnicode:')[1].split('\n')[0]})`
 
-        let general = map.split('[General]')[1].split('[')[0]
-
-        let diff = map.split('[Difficulty]')[1].split('[')[0]
-
-        let title = metadata.split('Title:')[1].split('\n')[0]
-            ==
-            metadata.split('Title:')[1].split('\n')[0]
-            ?
-            metadata.split('TitleUnicode:')[1].split('\n')[0] :
-            `${metadata.split('Title:')[1].split('\n')[0]} (${metadata.split('TitleUnicode:')[1].split('\n')[0]})`
-        let artist = metadata.split('Artist:')[1].split('\n')[0]
-            ==
-            metadata.split('ArtistUnicode:')[1].split('\n')[0]
-            ?
-            metadata.split('ArtistUnicode:')[1].split('\n')[0] :
-            `${metadata.split('Artist:')[1].split('\n')[0]} (${metadata.split('ArtistUnicode:')[1].split('\n')[0]})`
-
-        let creator = metadata.split('Creator:')[1].split('\n')[0]
-        let version = metadata.split('Version:')[1].split('\n')[0]
-
+            creator = metadata.split('Creator:')[1].split('\n')[0]
+            version = metadata.split('Version:')[1].split('\n')[0]
+        } catch (error) {
+            await message.reply({
+                content: `Error - invalid .osu file`,
+                allowedMentions: { repliedUser: false },
+            })
+            return;
+        }
         ftstr = `${artist} - ${title} [${version}] //${creator} ${mods ? `+${mods}` : ''}`
-
-        let hitobjs = map.split('[HitObjects]')[1].split('\n')
+        let hitobjs;
+        try {
+            hitobjs = map.split('[HitObjects]')[1].split('\n')
+        } catch (error) {
+            await message.reply({
+                content: `Error - invalid .osu file`,
+                allowedMentions: { repliedUser: false },
+            })
+            return;
+        }
         let countcircle = 0
         let countslider = 0
         let countspinner = 0
         //to get count_circle, get every line without a |
-        for (let i = 0; i < hitobjs.length; i++) {
-            let curobj = hitobjs[i]
-            if (curobj.includes('|')) {
-                countslider++
-            } else if (curobj.split(',').length > 5) {
-                countspinner++
-            } else {
-                countcircle++
+        try {
+            for (let i = 0; i < hitobjs.length; i++) {
+                let curobj = hitobjs[i]
+                if (curobj.includes('|')) {
+                    countslider++
+                } else if (curobj.split(',').length > 5) {
+                    countspinner++
+                } else {
+                    countcircle++
+                }
             }
+        } catch (error) {
+            await message.reply({
+                content: `Error - invalid .osu file`,
+                allowedMentions: { repliedUser: false },
+            })
+            return;
         }
-        let firsttimep = hitobjs[1].split(',')[2]
-        let fintimep = hitobjs[hitobjs.length - 2].split(',')[2] //inaccurate cos of sliders n stuff
 
+        let firsttimep;
+        let fintimep;
+        try {
+            firsttimep = hitobjs[1].split(',')[2]
+            fintimep = hitobjs[hitobjs.length - 2].split(',')[2] //inaccurate cos of sliders n stuff
+        } catch (error) {
+            await message.reply({
+                content: `Error - invalid .osu file`,
+                allowedMentions: { repliedUser: false },
+            })
+            return;
+        }
         let mslen = parseInt(fintimep) - parseInt(firsttimep)
 
         let nlength = mslen / 1000
@@ -118,8 +189,16 @@ overrideID: ${overrideID}
                 '00:' + Math.floor(nlength) //false
 
         let combocolours = []
-        let colours = map.split('[Colours]')[1].split('[')[0].split('\n')
-
+        let colours;
+        try {
+            colours = map.split('[Colours]')[1].split('[')[0].split('\n')
+        } catch (error) {
+            await message.reply({
+                content: `Error - invalid .osu file`,
+                allowedMentions: { repliedUser: false },
+            })
+            return;
+        }
         for (let i = 0; i < colours.length; i++) {
 
             try {
@@ -132,8 +211,16 @@ overrideID: ${overrideID}
 
         let bpm = NaN
 
-        let timing = map.split('[TimingPoints]')[1].split('[')[0]
-
+        let timing;
+        try {
+            timing = map.split('[TimingPoints]')[1].split('[')[0]
+        } catch (error) {
+            await message.reply({
+                content: `Error - invalid .osu file`,
+                allowedMentions: { repliedUser: false },
+            })
+            return;
+        }
         function pointToBPM(point: string) {
             let arr = point.split(',')
             //'a,b,c'
@@ -165,19 +252,38 @@ overrideID: ${overrideID}
 
             ;
         let gm = general.split('Mode:')[1].split('\n')[0].replaceAll(' ', '')
-        let strains = await osufunc.straincalclocal(null, mods, 0, osucalc.ModeIntToName(parseInt(gm)))
+
+        let strains;
+        let mapgraph
+        try {
+            strains = await osufunc.straincalclocal(null, mods, 0, osucalc.ModeIntToName(parseInt(gm)))
+        } catch (error) {
+            await message.reply({
+                content: `Error - invalid .osu file`,
+                allowedMentions: { repliedUser: false },
+            })
+            return;
+        }
+
         //fs.writeFileSync(`./debugosu/link-maplocal=strains=${obj.guildId}.json`, JSON.stringify(strains, null, 2))
-
-        let mapgraph = await osufunc.graph(strains.strainTime, strains.value, 'Strains', null, null, null, null, null, 'strains')
-
-
-        let osuEmbed = new Discord.EmbedBuilder()
-            .setTitle(`${ftstr}`)
-            .addFields([
-                {
-                    name: 'MAP VALUES',
-                    value:
-                        `
+        try {
+            mapgraph = await osufunc.graph(strains.strainTime, strains.value, 'Strains', null, null, null, null, null, 'strains')
+        } catch (error) {
+            await message.reply({
+                content: `Error - invalid .osu file`,
+                allowedMentions: { repliedUser: false },
+            })
+            return;
+        }
+        let osuEmbed;
+        try {
+            osuEmbed = new Discord.EmbedBuilder()
+                .setTitle(`${ftstr}`)
+                .addFields([
+                    {
+                        name: 'MAP VALUES',
+                        value:
+                            `
 CS${diff.split('CircleSize:')[1].split('\n')[0]} AR${diff.split('ApproachRate:')[1].split('\n')[0]} OD${diff.split('OverallDifficulty:')[1].split('\n')[0]} HP${diff.split('HPDrainRate:')[1].split('\n')[0]}
 ${emojis.mapobjs.circle}${countcircle}
 ${emojis.mapobjs.slider}${countslider}
@@ -185,22 +291,28 @@ ${emojis.mapobjs.spinner}${countspinner}
 ${emojis.mapobjs.total_length}${truelen}
 ${emojis.mapobjs.bpm}${bpmmax.toFixed(2)} - ${bpmmin.toFixed(2)} (${bpmavg.toFixed(2)})
 `,
-                    inline: true
-                },
-                {
-                    name: 'PP',
-                    value:
-                        `SS: ${ppcalcing[0].pp.toFixed(2)} \n ` +
-                        `99: ${ppcalcing[1].pp.toFixed(2)} \n ` +
-                        `98: ${ppcalcing[2].pp.toFixed(2)} \n ` +
-                        `97: ${ppcalcing[3].pp.toFixed(2)} \n ` +
-                        `96: ${ppcalcing[4].pp.toFixed(2)} \n ` +
-                        `95: ${ppcalcing[5].pp.toFixed(2)} \n `,
-                    inline: true
-                }
-            ])
-            .setImage(`${mapgraph}`)
-
+                        inline: true
+                    },
+                    {
+                        name: 'PP',
+                        value:
+                            `SS: ${ppcalcing[0].pp.toFixed(2)} \n ` +
+                            `99: ${ppcalcing[1].pp.toFixed(2)} \n ` +
+                            `98: ${ppcalcing[2].pp.toFixed(2)} \n ` +
+                            `97: ${ppcalcing[3].pp.toFixed(2)} \n ` +
+                            `96: ${ppcalcing[4].pp.toFixed(2)} \n ` +
+                            `95: ${ppcalcing[5].pp.toFixed(2)} \n `,
+                        inline: true
+                    }
+                ])
+                .setImage(`${mapgraph}`)
+        } catch (error) {
+            await message.reply({
+                content: `Error - invalid .osu file`,
+                allowedMentions: { repliedUser: false },
+            })
+            return;
+        }
         message.reply({
             embeds: [osuEmbed],
             allowedMentions: { repliedUser: false },
