71a9ab9c2df82ae465c19ea738fcaed2fb7d69f2
SaberStrike <71379681+sbrstrkkdwmdr@users.noreply.github.com>
2022-09-04 22:47:23 +1000

    commands
    - 8ball
    - gif
    - image search (WIP)

---
+++ commands/misc/8ball.ts
@@ -0,0 +1,130 @@
+import cmdchecks = require('../../src/checks');
+import fs = require('fs');
+import calc = require('../../src/calc');
+import emojis = require('../../src/consts/emojis');
+import colours = require('../../src/consts/colours');
+import osufunc = require('../../src/osufunc');
+import osumodcalc = require('osumodcalculator');
+import osuApiTypes = require('../../src/types/osuApiTypes');
+import Discord = require('discord.js');
+import log = require('../../src/log');
+
+module.exports = {
+    name: '8ball',
+    execute(commandType, obj, args, button, config, client, absoluteID, currentDate, overrides) {
+        let commanduser;
+
+        switch (commandType) {
+            case 'message': {
+                commanduser = obj.author;
+            }
+                break;
+
+            //==============================================================================================================================================================================================
+
+            case 'interaction': {
+                commanduser = obj.member.user;
+            }
+
+                //==============================================================================================================================================================================================
+
+                break;
+            case 'button': {
+                commanduser = obj.member.user;
+            }
+                break;
+        }
+        if (overrides != null) {
+
+        }
+
+        //==============================================================================================================================================================================================
+
+        const buttons: Discord.ActionRowBuilder = new Discord.ActionRowBuilder()
+            .addComponents(
+                new Discord.ButtonBuilder()
+                    .setCustomId(`BigLeftArrow-8ball-${commanduser.id}`)
+                    .setStyle(Discord.ButtonStyle.Primary)
+                    .setEmoji('⬅')
+                /* .setLabel('Start') */,
+                new Discord.ButtonBuilder()
+                    .setCustomId(`LeftArrow-8ball-${commanduser.id}`)
+                    .setStyle(Discord.ButtonStyle.Primary)
+                    .setEmoji('◀'),
+                new Discord.ButtonBuilder()
+                    .setCustomId(`RightArrow-8ball-${commanduser.id}`)
+                    .setStyle(Discord.ButtonStyle.Primary)
+                    .setEmoji('▶')
+                /* .setLabel('Next') */,
+                new Discord.ButtonBuilder()
+                    .setCustomId(`BigRightArrow-8ball-${commanduser.id}`)
+                    .setStyle(Discord.ButtonStyle.Primary)
+                    .setEmoji('➡')
+                /* .setLabel('End') */,
+            );
+
+        fs.appendFileSync(`logs/cmd/commands${obj.guildId}.log`,
+            log.commandLog(
+                '8ball',
+                commandType,
+                absoluteID,
+                commanduser
+            ), 'utf-8')
+        //OPTIONS==============================================================================================================================================================================================
+
+        fs.appendFileSync(`logs/cmd/commands${obj.guildId}.log`,
+            log.optsLog(
+                absoluteID,
+                []
+            ), 'utf-8')
+
+        //ACTUAL COMMAND STUFF==============================================================================================================================================================================================
+
+        const responses = [
+            'yes', 'no', 'What? no', '知らない', 'nope', 'yeahhh', 'a strong maybe', 'definitely maybe not', 'nah', 'yeah of course', '多分', '絶対!!!',
+            'come again?', 'ehhhh', '⠀', '💀', '🥺', 'bruhhh', 'splish splash your question is trash', 3
+        ]
+
+        //SEND/EDIT MSG==============================================================================================================================================================================================
+
+        switch (commandType) {
+            case 'message': case 'interaction': {
+                obj.reply({
+                    content: `${responses[Math.floor(Math.random() * responses.length)]}`,
+                    embeds: [],
+                    files: [],
+                    allowedMentions: { repliedUser: false },
+                    failIfNotExists: true
+                })
+                    .catch();
+            }
+                break;
+
+            //==============================================================================================================================================================================================
+
+            //==============================================================================================================================================================================================
+
+            case 'button': {
+                obj.edit({
+                    content: '',
+                    embeds: [],
+                    files: [],
+                    allowedMentions: { repliedUser: false },
+                    failIfNotExists: true
+                })
+                    .catch();
+            }
+                break;
+        }
+
+
+
+        fs.appendFileSync(`logs/cmd/commands${obj.guildId}.log`,
+            `
+----------------------------------------------------
+success
+ID: ${absoluteID}
+----------------------------------------------------
+\n\n`, 'utf-8')
+    }
+}
---
+++ commands/misc/image.ts
@@ -0,0 +1,196 @@
+import cmdchecks = require('../../src/checks');
+import fs = require('fs');
+import calc = require('../../src/calc');
+import emojis = require('../../src/consts/emojis');
+import colours = require('../../src/consts/colours');
+import osufunc = require('../../src/osufunc');
+import osumodcalc = require('osumodcalculator');
+import osuApiTypes = require('../../src/types/osuApiTypes');
+import extypes = require('../../src/types/extraTypes');
+import Discord = require('discord.js');
+import log = require('../../src/log');
+import fetch from 'node-fetch';
+
+module.exports = {
+    name: 'image',
+    async execute(commandType, obj, args, button, config, client, absoluteID, currentDate, overrides) {
+        let commanduser;
+        let query;
+        let iserr = false;
+
+        switch (commandType) {
+            case 'message': {
+                commanduser = obj.author;
+                query = args.join(' ');
+                if (!args[0]) {
+                    iserr = true;
+                }
+            }
+                break;
+
+            //==============================================================================================================================================================================================
+
+            case 'interaction': {
+                commanduser = obj.member.user;
+                query = obj.options.getString('query')
+            }
+
+                //==============================================================================================================================================================================================
+
+                break;
+            case 'button': {
+                commanduser = obj.member.user;
+            }
+                break;
+        }
+        if (overrides != null) {
+
+        }
+
+        //==============================================================================================================================================================================================
+
+        const buttons: Discord.ActionRowBuilder = new Discord.ActionRowBuilder()
+            .addComponents(
+                new Discord.ButtonBuilder()
+                    .setCustomId(`BigLeftArrow-image-${commanduser.id}`)
+                    .setStyle(Discord.ButtonStyle.Primary)
+                    .setEmoji('⬅')
+                /* .setLabel('Start') */,
+                new Discord.ButtonBuilder()
+                    .setCustomId(`LeftArrow-image-${commanduser.id}`)
+                    .setStyle(Discord.ButtonStyle.Primary)
+                    .setEmoji('◀'),
+                new Discord.ButtonBuilder()
+                    .setCustomId(`RightArrow-image-${commanduser.id}`)
+                    .setStyle(Discord.ButtonStyle.Primary)
+                    .setEmoji('▶')
+                /* .setLabel('Next') */,
+                new Discord.ButtonBuilder()
+                    .setCustomId(`BigRightArrow-image-${commanduser.id}`)
+                    .setStyle(Discord.ButtonStyle.Primary)
+                    .setEmoji('➡')
+                /* .setLabel('End') */,
+            );
+
+        fs.appendFileSync(`logs/cmd/commands${obj.guildId}.log`,
+            log.commandLog(
+                'image',
+                commandType,
+                absoluteID,
+                commanduser
+            ), 'utf-8')
+
+        //OPTIONS==============================================================================================================================================================================================
+
+        fs.appendFileSync(`logs/cmd/commands${obj.guildId}.log`,
+            log.optsLog(
+                absoluteID,
+                [{
+                    name: 'Query',
+                    value: query
+                }]
+            ), 'utf-8')
+
+        //ACTUAL COMMAND STUFF==============================================================================================================================================================================================
+
+        const res = await fetch(
+            `https://customsearch.googleapis.com/customsearch/v1?q=${query}&cx=${config.google.cx}&key=${config.google.apiKey}&searchType=image`
+        )
+
+
+        if (!res || res.status !== 200) {
+            obj.reply({
+                content: 'Error: could not fetch the requested image.',
+                allowedMentions: { repliedUser: false },
+                failIfNotExists: true
+            })
+                .catch(error => { });
+            return;
+        }
+
+        const response: extypes.imagesearches = await res.json() as any;
+
+        if (!response.items) {
+            obj.reply({
+                content: `Error: no results found for \`${query}\``,
+                allowedMentions: { repliedUser: false },
+                failIfNotExists: true
+            })
+                .catch(error => { });
+            return;
+        }
+
+        let resimg = '';
+        let i: number;
+        for (i = 0; i < response.items.length && i < 5; i++) {
+            resimg += `\n\n<${response.items[i].link}>`
+        }
+
+        const imageEmbed = new Discord.EmbedBuilder()
+            .setURL(`${'https://www.google.com/search?q=' + query}`)
+            .setTitle(`IMAGE RESULTS FOR ${query}`)
+            .setDescription(`(NOTE - links may be unsafe)\n${resimg}`)
+            .setColor(1)
+        const useEmbeds = [imageEmbed];
+
+        for (let i = 0; i < 5; i++) {
+            const curimg = new Discord.EmbedBuilder()
+                .setURL(`${'https://www.google.com/search?q=' + query.replaceAll(' ', '+')}`)
+                .setImage(`${response.items[i].image.thumbnailLink}`)
+            useEmbeds.push(curimg);
+        }
+
+        //SEND/EDIT MSG==============================================================================================================================================================================================
+        switch (commandType) {
+            case 'message': {
+                obj.reply({
+                    content: '',
+                    embeds: [useEmbeds],
+                    files: [],
+                    allowedMentions: { repliedUser: false },
+                    failIfNotExists: true
+                })
+                    .catch();
+            }
+                break;
+
+            //==============================================================================================================================================================================================
+
+            case 'interaction': {
+                obj.reply({
+                    content: '',
+                    embeds: [useEmbeds],
+                    files: [],
+                    allowedMentions: { repliedUser: false },
+                    failIfNotExists: true
+                })
+                    .catch();
+            }
+
+                //==============================================================================================================================================================================================
+
+                break;
+            case 'button': {
+                obj.edit({
+                    content: '',
+                    embeds: [],
+                    files: [],
+                    allowedMentions: { repliedUser: false },
+                    failIfNotExists: true
+                })
+                    .catch();
+            }
+                break;
+        }
+
+
+
+        fs.appendFileSync(`logs/cmd/commands${obj.guildId}.log`,
+            `
+----------------------------------------------------
+success
+ID: ${absoluteID}
+----------------------------------------------------
+\n\n`, 'utf-8')
+    }
+}
