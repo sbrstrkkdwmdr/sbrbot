da4529d6e0d13636b271dcf8e3178be3f9f8dfd1
SaberStrike <71379681+sbrstrkkdwmdr@users.noreply.github.com>
2022-09-04 22:48:16 +1000

    empty command files (will finish later)

---
+++ commands/misc/ytsearch.ts
@@ -0,0 +1,140 @@
+import cmdchecks = require('../../src/checks');
+import fs = require('fs');
+import calc = require('../../src/calc');
+import emojis = require('../../src/consts/emojis');
+import colours = require('../../src/consts/colours');
+import osufunc = require('../../src/osufunc');
+import osumodcalc = require('osumodcalculator');
+import osuApiTypes = require('../../src/types/osuApiTypes');
+import Discord = require('discord.js');
+import log = require('../../src/log');
+
+module.exports = {
+    name: 'ytsearch',
+    execute(commandType, obj, args, button, config, client, absoluteID, currentDate, overrides) {
+        let commanduser;
+        let baseCommandType;
+
+        switch (commandType) {
+            case 'message': {
+                commanduser = obj.author;
+            }
+                break;
+
+            //==============================================================================================================================================================================================
+
+            case 'interaction': {
+                commanduser = obj.member.user;
+            }
+
+            //==============================================================================================================================================================================================
+
+                break;
+            case 'button': {
+                commanduser = obj.member.user;
+            }
+                break;
+        }
+        if (overrides != null) {
+
+        }
+
+        //==============================================================================================================================================================================================
+
+        const buttons: Discord.ActionRowBuilder = new Discord.ActionRowBuilder()
+            .addComponents(
+                new Discord.ButtonBuilder()
+                    .setCustomId(`BigLeftArrow-ytsearch-${commanduser.id}`)
+                    .setStyle(Discord.ButtonStyle.Primary)
+                    .setEmoji('⬅')
+                /* .setLabel('Start') */,
+                new Discord.ButtonBuilder()
+                    .setCustomId(`LeftArrow-ytsearch-${commanduser.id}`)
+                    .setStyle(Discord.ButtonStyle.Primary)
+                    .setEmoji('◀'),
+                new Discord.ButtonBuilder()
+                    .setCustomId(`RightArrow-ytsearch-${commanduser.id}`)
+                    .setStyle(Discord.ButtonStyle.Primary)
+                    .setEmoji('▶')
+                /* .setLabel('Next') */,
+                new Discord.ButtonBuilder()
+                    .setCustomId(`BigRightArrow-ytsearch-${commanduser.id}`)
+                    .setStyle(Discord.ButtonStyle.Primary)
+                    .setEmoji('➡')
+                /* .setLabel('End') */,
+            );
+
+        fs.appendFileSync(`logs/cmd/commands${obj.guildId}.log`,
+            log.commandLog(
+                'ytsearch',
+                commandType,
+                absoluteID,
+                commanduser
+            ), 'utf-8')
+
+        //OPTIONS==============================================================================================================================================================================================
+
+        fs.appendFileSync(`logs/cmd/commands${obj.guildId}.log`,
+            log.optsLog(
+                absoluteID,
+                []
+            ), 'utf-8')
+
+        //ACTUAL COMMAND STUFF==============================================================================================================================================================================================
+
+
+
+        //SEND/EDIT MSG==============================================================================================================================================================================================
+        switch (commandType) {
+            case 'message': {
+                obj.reply({
+                    content: '',
+                    embeds: [],
+                    files: [],
+                    allowedMentions: { repliedUser: false },
+                    failIfNotExists: true
+                })
+                    .catch();
+            }
+                break;
+
+            //==============================================================================================================================================================================================
+           
+            case 'interaction': {
+                obj.reply({
+                    content: '',
+                    embeds: [],
+                    files: [],
+                    allowedMentions: { repliedUser: false },
+                    failIfNotExists: true
+                })
+                    .catch();
+            }
+
+            //==============================================================================================================================================================================================
+
+                break;
+            case 'button': {
+                obj.edit({
+                    content: '',
+                    embeds: [],
+                    files: [],
+                    allowedMentions: { repliedUser: false },
+                    failIfNotExists: true
+                })
+                    .catch();
+            }
+                break;
+        }
+
+
+        
+        fs.appendFileSync(`logs/cmd/commands${obj.guildId}.log`,
+            `
+----------------------------------------------------
+success
+ID: ${absoluteID}
+----------------------------------------------------
+\n\n`, 'utf-8')
+    }
+}
