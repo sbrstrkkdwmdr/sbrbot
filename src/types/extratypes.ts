bdd2958308bcca5223ee76b313ae6b79e3bbe3ed
SaberStrike <71379681+sbrstrkkdwmdr@users.noreply.github.com>
2022-09-04 22:48:02 +1000

    Update extratypes.ts

--- src/types/extratypes.ts
+++ src/types/extratypes.ts
@@ -74,6 +74,70 @@ type guildSettings = {
 
 }
 
+type imagesearches = {
+    kind: string,
+    url: {
+        type: string,
+        template: string
+    },
+    queries: {
+        request: {
+            title: string,
+            totalResults: string,
+            searchTerms: string,
+            count: number,
+            startIndex: number,
+            inputEncoding: string,
+            outputEncoding: string,
+            safe: string,
+            cx: string
+            searchType: string,
+        }[],
+        nextPage: {
+            title: string,
+            totalResults: string,
+            searchTerms: string,
+            count: number,
+            startIndex: number,
+            inputEncoding: string,
+            outputEncoding: string,
+            safe: string,
+            cx: string
+            searchType: string,
+        }[]
+    },
+    context: {
+        title: string
+    },
+    searchInformation: {
+        searchTime: number,
+        formattedSearchTime: string,
+        totalResults: string,
+        formattedTotalResults: string
+    },
+    items: googleSearchItem[]
+}
 
-export { config, guildSettings }
+type googleSearchItem = {
+    kind: string,
+    title: string,
+    htmlTitle: string,
+    link: string,
+    displayLink: string,
+    snippet: string,
+    htmlSnippet: string,
+    mime: string,
+    fileFormat: string,
+    image?: {
+        contextLink: string,
+        height: number,
+        width: number,
+        byteSize: number,
+        thumbnailLink: string,
+        thumbnailHeight: number,
+        thumbnailWidth: number
+    }
+}
+
+export { config, guildSettings, imagesearches }
 
