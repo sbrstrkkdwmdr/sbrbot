aa3c0e625e91be4db2d6e5b27b85053b9cecd971
SaberStrike <71379681+sbrstrkkdwmdr@users.noreply.github.com>
2022-09-14 18:54:28 +1000

    Update checkperms.ts

--- commands/admin/checkperms.ts
+++ commands/admin/checkperms.ts
@@ -10,14 +10,25 @@ import Discord = require('discord.js');
 import log = require('../../src/log');
 
 module.exports = {
-    name: 'COMMANDNAME',
+    name: 'checkperms',
     execute(commandType, obj, args, button, config, client, absoluteID, currentDate, overrides, userdata) {
         let commanduser;
-        let baseCommandType;
+        let searchUser;
+        let type = 'user';
 
         switch (commandType) {
             case 'message': {
                 commanduser = obj.author;
+                if (args[0]) {
+                    if (obj.mentions.users.size > 0) {
+                        searchUser = obj.mentions.users.first();
+                    } else {
+                        type = 'int'
+                        searchUser = client.users.cache.get(args.join(' '))
+                    }
+                } else {
+                    searchUser = commanduser;
+                }
             }
                 break;
 
@@ -25,9 +36,14 @@ module.exports = {
 
             case 'interaction': {
                 commanduser = obj.member.user;
+                if (obj.options.getUser('user')) {
+                    searchUser = obj.options.getUser('user');
+                } else {
+                    searchUser = commanduser;
+                }
             }
 
-            //==============================================================================================================================================================================================
+                //==============================================================================================================================================================================================
 
                 break;
             case 'button': {
@@ -44,21 +60,21 @@ module.exports = {
         const buttons: Discord.ActionRowBuilder = new Discord.ActionRowBuilder()
             .addComponents(
                 new Discord.ButtonBuilder()
-                    .setCustomId(`BigLeftArrow-COMMANDNAME-${commanduser.id}`)
+                    .setCustomId(`BigLeftArrow-checkperms-${commanduser.id}`)
                     .setStyle(Discord.ButtonStyle.Primary)
                     .setEmoji('⬅')
                 /* .setLabel('Start') */,
                 new Discord.ButtonBuilder()
-                    .setCustomId(`LeftArrow-COMMANDNAME-${commanduser.id}`)
+                    .setCustomId(`LeftArrow-checkperms-${commanduser.id}`)
                     .setStyle(Discord.ButtonStyle.Primary)
                     .setEmoji('◀'),
                 new Discord.ButtonBuilder()
-                    .setCustomId(`RightArrow-COMMANDNAME-${commanduser.id}`)
+                    .setCustomId(`RightArrow-checkperms-${commanduser.id}`)
                     .setStyle(Discord.ButtonStyle.Primary)
                     .setEmoji('▶')
                 /* .setLabel('Next') */,
                 new Discord.ButtonBuilder()
-                    .setCustomId(`BigRightArrow-COMMANDNAME-${commanduser.id}`)
+                    .setCustomId(`BigRightArrow-checkperms-${commanduser.id}`)
                     .setStyle(Discord.ButtonStyle.Primary)
                     .setEmoji('➡')
                 /* .setLabel('End') */,
@@ -66,7 +82,7 @@ module.exports = {
 
         fs.appendFileSync(`logs/cmd/commands${obj.guildId}.log`,
             log.commandLog(
-                'COMMANDNAME',
+                'checkperms',
                 commandType,
                 absoluteID,
                 commanduser
@@ -82,6 +98,29 @@ module.exports = {
 
         //ACTUAL COMMAND STUFF==============================================================================================================================================================================================
 
+        if (searchUser == null || typeof searchUser == 'undefined') {
+            searchUser = commanduser;
+        }
+
+        const embed = new Discord.EmbedBuilder()
+        try {
+            const userAsMember = obj.guild.members.cache.get(searchUser.id);
+            //get perms
+            const perms = userAsMember.permissions.toArray().join(' **|** ');
+
+            embed
+                .setTitle(`${searchUser.username}'s Permissions`)
+                .setDescription(`**${perms}**`)
+                .setColor(colours.embedColour.admin.dec)
+
+        } catch (err) {
+            embed.setTitle('Error')
+                .setDescription('An error occured while trying to get the permissions of the user.')
+                .setColor(colours.embedColour.admin.dec)
+
+        }
+
+
 
 
         //SEND/EDIT MSG==============================================================================================================================================================================================
@@ -89,7 +128,7 @@ module.exports = {
             case 'message': {
                 obj.reply({
                     content: '',
-                    embeds: [],
+                    embeds: [embed],
                     files: [],
                     allowedMentions: { repliedUser: false },
                     failIfNotExists: true
@@ -99,11 +138,11 @@ module.exports = {
                 break;
 
             //==============================================================================================================================================================================================
-           
+
             case 'interaction': {
                 obj.reply({
                     content: '',
-                    embeds: [],
+                    embeds: [embed],
                     files: [],
                     allowedMentions: { repliedUser: false },
                     failIfNotExists: true
@@ -111,7 +150,7 @@ module.exports = {
                     .catch();
             }
 
-            //==============================================================================================================================================================================================
+                //==============================================================================================================================================================================================
 
                 break;
             case 'button': {
@@ -128,7 +167,7 @@ module.exports = {
         }
 
 
-        
+
         fs.appendFileSync(`logs/cmd/commands${obj.guildId}.log`,
             `
 ----------------------------------------------------
