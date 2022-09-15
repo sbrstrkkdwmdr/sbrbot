181e574599ff1dff85776215e4a7efe9a789749d
SaberStrike <71379681+sbrstrkkdwmdr@users.noreply.github.com>
2022-09-15 20:20:57 +1000

    💀

--- src/other.ts
+++ src/other.ts
@@ -36,204 +36,318 @@ export function readAllFiles(directory: string) {
 }
 
 export function flagImgUrl(string: string, ver?: 'osu') {
-    let flagUrl: string = 'https://osu.ppy.sh/assets/images/flags/';
-
-    //sorted by two letter codes
-
-    switch (string.toLowerCase()) {
-        case 'ar': case 'argentina':
-            flagUrl += '';
-            break;
-        case 'at': case 'austria':
-            flagUrl += '';
-            break;
-        case 'az': case 'azerbaijan':
-            flagUrl += '';
-            break;
-        case 'au': case 'australia':
-            flagUrl += '1f1e6-1f1fa.svg';
-            break;
-        case 'bh': case 'bahrain':
-            flagUrl += '';
-            break;
-        case 'bb': case 'barbados':
-            flagUrl += '';
-            break;
-        case 'bd': case 'bangladesh':
-            flagUrl += '';
-            break;
-        case 'be': case 'belgium':
-            flagUrl += '';
-            break;
-        case 'bn': case 'brunei':
-            flagUrl += '';
-            break;
-        case 'br': case 'brazil':
-            flagUrl += '';
-            break;
-        case 'bt': case 'bhutan':
-            flagUrl += '';
-            break;
-        case 'bw': case 'botswana':
-            flagUrl += '';
-            break;
-        case 'by': case 'belarus':
-            flagUrl += '';
-            break;
-        case 'de': case 'germany':
-            flagUrl += '';
-            break;
-        case 'dz': case 'algeria':
-            flagUrl += '';
-            break;
-        case 'fr': case 'france':
-            flagUrl += '';
-            break;
-        case 'id': case 'indonesia':
-            flagUrl += '';
-            break;
-        case 'kr': case 'south korea': case 'republic of korea':
-            flagUrl += '';
-            break;
-        case 'my': case 'malaysia':
-            flagUrl += '';
-            break;
-        case 'ru': case 'russia':
-            flagUrl += '1f1f7-1f1fa.svg';
-            break;
-        case 'se': case 'sweden':
-            flagUrl += '';
-            break;
-        case 'sg': case 'singapore':
-            flagUrl += '';
-            break;
-        case 'us': case 'usa': case 'america': case 'united states': case 'united states of america':
-            flagUrl += '1f1fa-1f1f8.svg';
-            break;
-        case 'vn':
-            break;
-    }
-
-    return flagUrl;
-    // uk
-    // ----us
-    // ----algeria
-    // ----argentina
-    // ----australia
-    // ----austria
-    // ----azerbaijan
-    // --bahrain
-    // --bangladesh
-    // --barbados
-    // --belarus
-    // --belgium
-    // --bhutan
-    // --botswana
-    // brazil
-    // brunei
-    // bulgaria
-    // cambodia
-    // canda
-    // chile
-    // china
-    // colombia
-    // costa rica
-    // cote d'lvoire
-    // croatia
-    // cuba
-    // curacao
-    // cyprus
-    // czech
-    // denmark
-    // djibouti
-    // ecuador
-    // egypt
-    // el salvador
-    // estonia
-    // ethiopia
-    // fiji
-    // finland
-    // ----france
-    // gabon
-    // ghana
-    // greece
-    // guam
-    // honduras
-    // hong kong
-    // hungary
-    // iceland
-    // india
-    // ----indonesia
-    // iraq
-    // ireland
-    // islamic rep. o. iran
-    // israel
-    // italy
-    // jamaica
-    // japan
-    // jordan
-    // kenya
-    // kosov
-    // kuwait
-    // latvia
-    // liechtenstein
-    // lithuania
-    // luxembourg
-    // madagascar
-    // maldives
-    // malta
-    // ----malaysia
-    // mauritius
-    // mexico
-    // monaco
-    // mongolia
-    // morocco
-    // myanmar
-    // nepal
-    // netherlands
-    // new caledonia
-    // new zealand
-    // nigeria
-    // north macedonia
-    // norway
-    // oman
-    // pakistan
-    // panama
-    // papua new guinea
-    // paraguay
-    // peru
-    // philippines
-    // poland
-    // portugal
-    // qatar
-    // romania
-    // ----russia
-    // saudi arabia
-    // senegal
-    // sierra leone
-    // ----singapore
-    // slovakia
-    // slovenia 
-    // south africa
-    // ----south korea
-    // spain
-    // sri lanka
-    // sudan
-    // ----sweden
-    // switzerland
-    // syrian arab rep.
-    // taiwan
-    // thailand
-    // togo
-    // trinidad and tobago
-    // tunisia
-    // turkey
-    // ukraine
-    // uae
-    // united rep. o. tanzania
-    // uruguay
-    // venezuala 
-    // ----vietnam
-    // zimbabwe
-
+    let flagUrl: string = `https://osuflags.omkserver.nl/${string}`;
+return flagUrl;
 }
+    // forgor https://osuflags.omkserver.nl/ was a thing
+
+    
+
+
+//     //sorted by alphabetically
+
+//     switch (string.toLowerCase()) {
+//         case 'argentina': case 'ar': case 'arg':
+//             flagUrl += '';
+//             break;
+//         case 'algeria': case 'dz': case 'dza':
+//             flagUrl += '';
+//             break;
+//         case 'australia': case 'au': case 'aus':
+//             flagUrl += '1f1e6-1f1fa.svg';
+//             break;
+//         case 'austria': case 'at': case 'aut':
+//             flagUrl += '';
+//             break;
+//         case 'azerbaijan': case 'az': case 'aze':
+//             flagUrl += '';
+//             break;
+//         case 'bahrain': case 'bh': case 'bhr':
+//             flagUrl += '';
+//             break;
+//         case 'bangladesh': case 'bd': case 'bgd':
+//             flagUrl += '';
+//             break;
+//         case 'barbados': case 'bb': case 'brb':
+//             flagUrl += '';
+//             break;
+//         case 'belarus': case 'by': case 'blr':
+//             flagUrl += '';
+//             break;
+//         case 'belgium': case 'be': case 'bel':
+//             flagUrl += '';
+//             break;
+//         case 'bhutan': case 'bt': case 'btn':
+//             flagUrl += '';
+//             break;
+//         case 'brazil': case 'br': case 'bra':
+//             flagUrl += '';
+//             break;
+//         case 'brunei': case 'bn': case 'brn':
+//             flagUrl += '';
+//             break;
+//         case 'botswana': case 'bw': case 'bwa':
+//             flagUrl += '';
+//             break;
+//         case 'bulgaria': case 'bg': case 'bgr':
+//             flagUrl += '';
+//             break;
+//         case 'cambodia': case 'kh': case 'khm':
+//             flagUrl += '';
+//             break;
+//         case 'canada': case 'ca': case 'can':
+//             flagUrl += '';
+//             break;
+//         case 'chile': case 'cl': case 'chl':
+//             flagUrl += '';
+//             break;
+//         case 'china': case 'cn': case 'chn':
+//             flagUrl += '';
+//             break;
+//         case 'colombia': case 'co': case 'col':
+//             flagUrl += '';
+//             break;
+//         case 'costa rica': case 'cr': case 'cri':
+//             flagUrl += '';
+//             break;
+//         case 'cote d\'ivoire': case 'ci': case 'civ': case 'ivory coast':
+//             flagUrl += '';
+//             break;
+//         case 'croatia': case 'hr': case 'hrv':
+//             flagUrl += '';
+//             break;
+//         case 'cuba': case 'cu': case 'cub':
+//             flagUrl += '';
+//             break;
+//         case 'curaçao': case 'cw': case 'cuw':
+//             flagUrl += '';
+//             break;
+//         case 'cyprus': case 'cy': case 'cyp':
+//             flagUrl += '';
+//             break;
+//         case 'czech republic': case 'cz': case 'cze': case 'czechia':
+//             flagUrl += '';
+//             break;
+//         case 'denmark': case 'dk': case 'dnk':
+//             flagUrl += '';
+//             break;
+//         case 'djibouti': case 'dj': case 'dji':
+//             flagUrl += '';
+//             break;
+//         case 'ecuador': case 'ec': case 'ecu':
+//             flagUrl += '';
+//             break;
+//         case 'egypt': case 'eg': case 'egy':
+//             flagUrl += '';
+//             break;
+//         case 'el salvador': case 'sv': case 'slv':
+//             flagUrl += '';
+//             break;
+//         case 'estonia': case 'ee': case 'est':
+//             flagUrl += '';
+//             break;
+//         case 'ethiopia': case 'et': case 'eth':
+//             flagUrl += '';
+//             break;
+//         case 'fiji': case 'fj': case 'fji':
+//             flagUrl += '';
+//             break;
+//         case 'finland': case 'fi': case 'fin':
+//             flagUrl += '';
+//             break;
+//         case 'france': case 'fr': case 'fra':
+//             flagUrl += '';
+//             break;
+//         case 'gabon': case 'ga': case 'gab':
+//             flagUrl += '';
+//             break;
+//         case 'germany': case 'de': case 'deu':
+//             flagUrl += '';
+//             break;
+//         case 'ghana': case 'gh': case 'gha':
+//             flagUrl += '';
+//             break;
+//         case 'greece': case 'gr': case 'grc':
+//             flagUrl += '';
+//             break;
+//         case 'guam': case 'gu': case 'gum':
+//             flagUrl += '';
+//             break;
+//         case 'honduras': case 'hn': case 'hnd':
+//             flagUrl += '';
+//             break;
+//         case 'hong kong': case 'hk': case 'hkg':
+//             flagUrl += '';
+//             break;
+//         case 'hungary': case 'hu': case 'hun':
+//             flagUrl += '';
+//             break;
+//         case 'iceland': case 'is': case 'isl':
+//             flagUrl += '';
+//             break;
+//         case 'india': case 'in': case 'ind':
+//             flagUrl += '';
+//             break;
+//         case 'indonesia': case 'id': case 'idn':
+//             flagUrl += '';
+//             break;
+//         case 'iran': case 'ir': case 'irn':
+//             flagUrl += '';
+//             break;
+//         case 'iraq': case 'iq': case 'irq':
+//             flagUrl += '';
+//             break;
+//         case 'ireland': case 'ie': case 'irl':
+//             flagUrl += '';
+//             break;
+//         case 'israel': case 'il': case 'isr':
+//             flagUrl += '';
+//             break;
+//         case 'italy': case 'it': case 'ita':
+//             flagUrl += '';
+//             break;
+//         case 'malaysia': case 'my':
+//             flagUrl += '';
+//             break;
+//         case 'south korea': case 'republic of korea': case 'kr':
+//             flagUrl += '';
+//             break;
+//         case 'russia': case 'ru':
+//             flagUrl += '1f1f7-1f1fa.svg';
+//             break;
+//         case 'sweden': case 'se':
+//             flagUrl += '';
+//             break;
+//         case 'singapore': case 'sg':
+//             flagUrl += '';
+//             break;
+//         case 'usa': case 'america': case 'united states': case 'united states of america': case 'us':
+//             flagUrl += '1f1fa-1f1f8.svg';
+//             break;
+//         case 'vn':
+//             break;
+//     }
+
+//     return flagUrl;
+//     // uk
+//     // ----us
+//     // ----algeria
+//     // ----argentina
+//     // ----australia
+//     // ----austria
+//     // ----azerbaijan
+//     // --bahrain
+//     // --bangladesh
+//     // --barbados
+//     // --belarus
+//     // --belgium
+//     // --bhutan
+//     // --botswana
+//     // --brazil
+//     // --brunei
+//     // --bulgaria
+//     // --cambodia
+//     // --canda
+//     // --chile
+//     // --china
+//     // --colombia
+//     // --costa rica
+//     // --cote d'lvoire
+//     // --croatia
+//     // --cuba
+//     // --curacao
+//     // --cyprus
+//     // --czech
+//     // --denmark
+//     // --djibouti
+//     // --ecuador
+//     // --egypt
+//     // --el salvador
+//     // --estonia
+//     // --ethiopia
+//     // --fiji
+//     // --finland
+//     // --france
+//     // --gabon
+//     // --ghana
+//     // --greece
+//     // --guam
+//     // --honduras
+//     // --hong kong
+//     // --hungary
+//     // --iceland
+//     // --india
+//     // ----indonesia
+//     // --iraq
+//     // --ireland
+//     // --islamic rep. o. iran
+//     // --israel
+//     // italy
+//     // jamaica
+//     // japan
+//     // jordan
+//     // kenya
+//     // kosov
+//     // kuwait
+//     // latvia
+//     // liechtenstein
+//     // lithuania
+//     // luxembourg
+//     // madagascar
+//     // maldives
+//     // malta
+//     // ----malaysia
+//     // mauritius
+//     // mexico
+//     // monaco
+//     // mongolia
+//     // morocco
+//     // myanmar
+//     // nepal
+//     // netherlands
+//     // new caledonia
+//     // new zealand
+//     // nigeria
+//     // north macedonia
+//     // norway
+//     // oman
+//     // pakistan
+//     // panama
+//     // papua new guinea
+//     // paraguay
+//     // peru
+//     // philippines
+//     // poland
+//     // portugal
+//     // qatar
+//     // romania
+//     // ----russia
+//     // saudi arabia
+//     // senegal
+//     // sierra leone
+//     // ----singapore
+//     // slovakia
+//     // slovenia 
+//     // south africa
+//     // ----south korea
+//     // spain
+//     // sri lanka
+//     // sudan
+//     // ----sweden
+//     // switzerland
+//     // syrian arab rep.
+//     // taiwan
+//     // thailand
+//     // togo
+//     // trinidad and tobago
+//     // tunisia
+//     // turkey
+//     // ukraine
+//     // uae
+//     // united rep. o. tanzania
+//     // uruguay
+//     // venezuala 
+//     // ----vietnam
+//     // zimbabwe
+
+// }
