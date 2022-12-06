3293ee645ab4bf4cb5cd3debd950334b5c5fd74b
SaberStrike <71379681+sbrstrkkdwmdr@users.noreply.github.com>
2022-12-06 20:00:47 +1100

    bump version to 0.4.2 20221206

--- src/consts/main.ts
+++ src/consts/main.ts
@@ -1,5 +1,5 @@
-export const version = '20221203';
+export const version = '20221206';
 
-export const versionAlt = '0.4.1';
+export const versionAlt = '0.4.2';
 
 export const linkInvite = 'There is currently no invite link publicly available';
