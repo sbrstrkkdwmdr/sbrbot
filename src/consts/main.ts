62148930521db14bc28b8ecc35bb897e072a955a
SaberStrike <71379681+sbrstrkkdwmdr@users.noreply.github.com>
2022-12-18 20:12:58 +1100

    read last commit

--- src/consts/main.ts
+++ src/consts/main.ts
@@ -1,5 +1,5 @@
-export const version = '20221213';
+export const version = '20221218';
 
-export const versionAlt = '0.4.4';
+export const versionAlt = '0.4.6';
 
 export const linkInvite = 'There is currently no invite link publicly available';
