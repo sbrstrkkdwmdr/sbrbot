ec60a52823a7720a8c9e3e6efbc9bb81e2b4a1e3
SaberStrike <sabersfear@gmail.com>
2023-01-12 14:02:55 +1100

    bump version

--- src/consts/main.ts
+++ src/consts/main.ts
@@ -1,5 +1,5 @@
-export const version = '20230104';
+export const version = '20230112';
 
-export const versionAlt = '0.4.8';
+export const versionAlt = '0.4.9';
 
 export const linkInvite = 'There is currently no invite link publicly available';
