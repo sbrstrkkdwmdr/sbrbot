1edceb066ef9f731bd219b6513ad6b1b888382d2
SaberStrike <sabersfear@gmail.com>
2023-01-25 09:04:11 +1100

    remove unstablerate
    maybe i'll re-add later?? idk

--- commands/cmdosu.ts
+++ commands/cmdosu.ts
@@ -5934,12 +5934,12 @@ export async function replayparse(input: extypes.commandInput) {
 
     const chart = await msgfunc.SendFileToChannel(input.graphChannel, await osufunc.graph(dataLabel, lifebarF, 'Health', null, null, null, null, null, 'replay'));
 
-    const UR =
-        mapdata.id ?
-            await osufunc.calcUr(
-                `${filespath}\\replays\\${input.absoluteID}.osr`,
-                `${path}/files/maps/${mapdata.id}.osu`
-            ) : null;
+    // const UR =
+    //     mapdata.id ?
+    //         await osufunc.calcUr(
+    //             `${filespath}\\replays\\${input.absoluteID}.osr`,
+    //             `${path}/files/maps/${mapdata.id}.osu`
+    //         ) : null;
 
     const Embed = new Discord.EmbedBuilder()
         .setFooter({
@@ -5954,7 +5954,6 @@ export async function replayparse(input: extypes.commandInput) {
             `
 ${replay.score.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")} | ${replay.max_combo.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}x/**${mapcombo}x** | ${accuracy.toFixed(2)}%
 \`${hitlist}\`
-${UR ? `${UR.unstablerate.toFixed(2)}UR | ${UR.averageEarly.toFixed(2)}ms - ${UR.averageLate.toFixed(2)}ms` : ''}
 ${xpp[0].pp.toFixed(2)}pp | ${xpp[1].pp.toFixed(2)}pp if ${fcacc.toFixed(2)}% FC 
 ${ppissue}
 ${isfail}
