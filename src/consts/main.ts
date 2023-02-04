15801df248b24e2b6d68da8fa208abb79db784cd
SaberStrike <sabersfear@gmail.com>
2023-02-05 09:51:48 +1100

    bump version to 0.4.11

--- src/consts/main.ts
+++ src/consts/main.ts
@@ -1,5 +1,5 @@
-export const version = '20230122';
+export const version = '20230205';
 
-export const versionAlt = '0.4.10';
+export const versionAlt = '0.4.11';
 
 export const linkInvite = 'There is currently no invite link publicly available';
