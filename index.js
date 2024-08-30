eb600c99becc64de4502afa97cea7c74cf6b0bb4
SaberStrike <sabersfear@gmail.com>
2024-08-30 14:13:54 +1000

    commands page
    to do: each thing scrolls by itself rather than whole page

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-function generateCommands() {
+function arrToAscii(string) {
+    return string.replaceAll('<', '&lt').replaceAll('>', '&gt');
+}
+
+function _generateCommands() {
     let gencmddiv = document.getElementById('generalcmd');
     let osucmddiv = document.getElementById('osucmd');
     let misccmddiv = document.getElementById('misccmd');
@@ -113,6 +117,183 @@ ${cmd?.linkusage && cmd.linkusage.length > 0 ?
             div.appendChild(cmddiv);
         }
     }
+}
+
+function generateCommands() {
+    let gencmddiv = document.getElementById('generalcmd');
+    let osucmddiv = document.getElementById('osucmd');
+    let misccmddiv = document.getElementById('misccmd');
+    let admincmddiv = document.getElementById('admincmd');
+    let buttondiv = document.getElementById('buttondiv');
+
+    toList(generalcommands, gencmddiv, 'generalcmd');
+    toList(osucommands, osucmddiv, 'osucmd');
+    toList(misccommands, misccmddiv, 'misccmd');
+    toList(admincommands, admincmddiv, 'admincmd');
+    toList(buttons, buttondiv, 'buttons');
+
+}
+
+/**
+ * 
+ * @param {generalcommands | buttons} commands 
+ * @param {*} div 
+ * @param {*} name 
+ */
+function toList(commands, div, name) {
+    for (const command of commands) {
+        let cmddiv = document.createElement('div')
+        cmddiv.classList.add('commandButton')
+        cmddiv.id = `${name}-${command.name}`
+        cmddiv.innerHTML = command.name;
+        cmddiv.addEventListener('click', () => {
+            document.body.scrollTop = 0; // For Safari
+            document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+            const mainDiv = document.getElementById('commandDetails');
+            if (name.includes('button')) {
+                displayButton(command, mainDiv);
+            } else {
+                displayCommand(command, mainDiv);
+            }
+            for (const div of document.getElementsByClassName('commandButton')) {
+                div.classList.remove('commandSelected');
+            }
+            cmddiv.classList.add('commandSelected');
+        })
+        div.appendChild(cmddiv);
+    }
+}
+
+/**
+ * 
+ * @param {generalcommands[0]} cmd 
+ */
+function displayCommand(cmd, mainDiv) {
+    mainDiv.innerHTML = '';
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    const title = document.createElement('h1');
+    title.innerHTML = cmd.name;
+    mainDiv.append(title);
+
+    const details = document.createElement('div');
+    details.classList.add('divCommandDetails')
+    const desc = document.createElement('p');
+    desc.innerHTML = cmd.description.includes('http') ?
+        urlToHTML(cmd.description) : cmd.description;
+
+    const usage = document.createElement('p');
+    usage.innerHTML = '<h3>Command:</h3>'
+    if (cmd.usage) {
+        const usediv = document.createElement('div');
+        usediv.classList.add('codeblock');
+        usediv.innerHTML = 'sbr-' + arrToAscii(cmd.usage);
+        usage.append(usediv, document.createElement('br'), document.createElement('br'));
+    }
+    if (cmd.slashusage) {
+        const usediv = document.createElement('div');
+        usediv.classList.add('codeblock');
+        usediv.innerHTML = '/' + arrToAscii(cmd.slashusage);
+        usage.append(usediv, document.createElement('br'), document.createElement('br'));
+    }
+    if (cmd.linkusage && cmd.linkusage.length > 0) {
+        for (const string of cmd.linkusage) {
+            const usediv = document.createElement('div');
+            usediv.classList.add('codeblock');
+            usediv.innerHTML = arrToAscii(string);
+            usage.append(usediv, document.createElement('br'), document.createElement('br'));
+        }
+    }
+    details.append(desc, usage);
+    if (cmd.aliases.length > 0) {
+        const aliasDiv = document.createElement('div');
+        aliasDiv.classList.add('fullClear')
+        const aliasName = document.createElement('h3');
+        aliasName.innerHTML = 'Aliases: '
+        aliasDiv.append(aliasName);
+        for (const alias of cmd.aliases) {
+            const div = document.createElement('div');
+            div.classList.add('codeblock');
+            div.innerHTML = alias;
+            aliasDiv.append(div)
+        }
+
+        details.append(aliasDiv);
+    }
+
+    if (cmd.examples.length > 0) {
+        const examplesDiv = document.createElement('div');
+        examplesDiv.classList.add('fullClear')
+        const exampleName = document.createElement('h3');
+        exampleName.innerHTML = 'Examples: '
+        examplesDiv.append(exampleName)
+        const exampleTable = document.createElement('table');
+        exampleTable.className = 'cmdexample'
+        for (const example of cmd.examples) {
+            const row = exampleTable.insertRow();
+            const cell1 = row.insertCell();
+            const cell2 = row.insertCell();
+            cell1.classList.add('tdEx', 'extxt');
+            cell1.innerHTML = example.text.replace('PREFIXMSG', 'sbr-');
+            cell2.classList.add('tdEx', 'exdesc');
+            cell2.innerHTML = example.descriptor;
+        }
+        examplesDiv.append(exampleTable);
+        details.append(examplesDiv);
+    }
+
+    const table = document.createElement('table');
+    table.classList.add('table');
+    table.insertRow();
+    table.rows[0].insertCell().innerHTML = 'Name';
+    table.rows[0].insertCell().innerHTML = 'Type';
+    table.rows[0].insertCell().innerHTML = 'Required';
+    table.rows[0].insertCell().innerHTML = 'Description';
+    table.rows[0].insertCell().innerHTML = 'Options';
+    table.rows[0].insertCell().innerHTML = 'Default Value';
+    table.rows[0].insertCell().innerHTML = 'Examples';
+    table.rows[0].insertCell().innerHTML = 'Command Types';
+    for (const option of cmd.options) {
+        const row = table.insertRow();
+        row.insertCell().innerHTML = option.name;
+        row.insertCell().innerHTML = option.type;
+        row.insertCell().innerHTML = option.required;
+        row.insertCell().innerHTML =
+            option.description.includes('[') && option.description.includes(']') ?
+                markdownURLtoHTML(option.description) + ', ' :
+                option.description.includes('http') ?
+                    urlToHTML(option.description) :
+                    option.description;
+        row.insertCell().innerHTML = option.options.map(opt =>
+            opt?.includes('[') && opt?.includes(']') && opt?.includes('(') && opt?.includes(')') ?
+                markdownURLtoHTML(opt) : opt).join(', ');
+        row.insertCell().innerHTML = option.defaultValue;
+        row.insertCell().innerHTML = option.examples ? option.examples.join('\n') : '';
+        row.insertCell().innerHTML = option.commandTypes ? option.commandTypes.join('\n') : '';
+    }
+    details.append(table)
+    mainDiv.append(details);
+}
+/**
+ * 
+ * @param {buttons[0]} button 
+ */
+function displayButton(button, mainDiv) {
+    mainDiv.innerHTML = '';
+    const title = document.createElement('h1');
+    title.innerHTML = button.name;
+    mainDiv.append(title);
+
+    const details = document.createElement('div');
+    details.classList.add('divCommandDetails')
+    const desc = document.createElement('p');
+    desc.innerHTML = button.description.includes('http') ?
+        urlToHTML(button.description) : button.description;
+    const emoji = document.createElement('img')
+    emoji.src = button.emoji;
+    emoji.alt = button.name;
+    details.append(desc, emoji);
+    mainDiv.append(details);
 }
 
 function markdownURLtoHTML(str) {
