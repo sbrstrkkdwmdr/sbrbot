1fd28b67a9c7f58a28468e5bd6d573c666f01200
SaberStrike <71379681+sbrstrkkdwmdr@users.noreply.github.com>
2024-10-14 10:41:43 +1100

    Update responses.ts

--- src/consts/responses.ts
+++ src/consts/responses.ts
@@ -1,17 +1,17 @@
 export const affirm = [
-    'yes', 'yeahhh', 'yeah of course', 'absolutely',
+    'yes', 'yeahhh', 'yeah of course', 'absolutely', 'HELL YEAH', 'yuh',
 ];
 
 export const negate = [
-    'no', 'What? no', 'definitely maybe not', 'nah', 'nope',
+    'no', 'What? no', 'definitely maybe not', 'nah', 'nope', 'nuh uh',
 ];
 
 export const huh = [
-    '知らない', 'a strong maybe', '多分', 'come again?', 'ehhhh', '⠀', 'ask me later',
+    '知らない', 'a strong maybe', '多分', 'come again?', 'ehhhh', '⠀', 'ask me later', 'what do you think 💀', 'huh?'
 ];
 
 export const other = [
-    '💀', '🥺', 'bruhhh', 'splish splash your question is trash', '3', 'why would you ask me that? what is wrong with you man...', '😭'
+    '💀', '🥺', 'bruhhh', 'splish splash your question is trash', '3', 'why would you ask me that? what is wrong with you man...', '😭', 'lemme think about that', 'idk what that word means', '英語話せません😢'
 ];
 
 export const decline = [
